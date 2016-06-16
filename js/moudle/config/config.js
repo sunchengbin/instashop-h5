30d84d1a1f4b4d2a5098bbc471280f7a09399264
sunchengbin <sunchengbin@hichao.com>
2016-06-16 19:03:13 +0800

    商品详情页

--- js/moudle/config/config.js
+++ js/moudle/config/config.js
@@ -15,7 +15,7 @@ define([],function(){
             //hrefUrl : 'http://mnew-test.instashop.co.id/html/'
             //测试环境
             hostUrl : 'http://m-test.instashop.co.id/js/',//内部router的路径
-            actionUrl : 'https://apip-test.instarekber.com/instashop/',
+            actionUrl : 'https://apip-test.instashop.co.id/instashop/',
             host : 'http://m-test.instashop.co.id/',
             imgUrl : 'http://m-test.instashop.co.id/images',
             hrefUrl : 'http://m-test.instashop.co.id/html/'
