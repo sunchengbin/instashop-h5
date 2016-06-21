3e6fb6970174355796f41ff9d0b56e8b457be414
sunchengbin <sunchengbin@weidian.com>
2016-06-21 14:39:34 +0800

    修改bug

--- html/detail.php
+++ html/detail.php
@@ -10,7 +10,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
   <meta name="apple-touch-fullscreen" content="yes"/>
   <meta name="apple-mobile-web-app-capable" content="yes"/>
   <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
-  <link href="<?=STATIC_HOST?>/css/dist/app/item.css?v=1466482343824" rel="stylesheet"/>
+  <link href="<?=STATIC_HOST?>/css/dist/app/item.css?v=1466490265508" rel="stylesheet"/>
   <title>Item detail</title>
     <?php
         include_once( dirname(__FILE__).'/../html/router/util.php' );
@@ -30,8 +30,8 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
         ?>
 </head>
 <body>
-  <script src="<?=STATIC_HOST?>/js/base/require-zepto.js?v=1466482343824"></script>
-  <script src="<?=STATIC_HOST?>/js/base/require-config.js?v=1466482343824"></script>
-  <script src="<?=STATIC_HOST?>/js/app/item.js?v=1466482343824"></script>
+  <script src="<?=STATIC_HOST?>/js/base/require-zepto.js?v=1466490265508"></script>
+  <script src="<?=STATIC_HOST?>/js/base/require-config.js?v=1466490265508"></script>
+  <script src="<?=STATIC_HOST?>/js/app/item.js?v=1466490265508"></script>
 </body>
 </html>
