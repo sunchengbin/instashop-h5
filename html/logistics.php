1ad9919406e1917c1fd7b73a08c2706e8faed2f5
sunchengbin <sunchengbin@weidian.com>
2016-06-23 15:40:13 +0800

    修改bug

--- html/logistics.php
+++ html/logistics.php
@@ -9,7 +9,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     <meta http-equiv="pragma" content="no-cache">
     <meta http-equiv="cache-control" content="no-cache">
     <meta http-equiv="expect" content="0">
-    <link href="<?=STATIC_HOST?>/css/dist/app/logistics.css?v=1466663055612" rel="stylesheet"/>
+    <link href="<?=STATIC_HOST?>/css/dist/app/logistics.css?v=1466667216098" rel="stylesheet"/>
     <title>Detail pengiriman</title>
     <script>
         <?php
@@ -33,9 +33,9 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
       </script>
 </head>
 <body>
-    <script src="<?=STATIC_HOST?>/js/base/require-zepto.js?v=1466663055612"></script>
+    <script src="<?=STATIC_HOST?>/js/base/require-zepto.js?v=1466667216098"></script>
     <!--<script src="../js/base/require-config.js"></script>-->
-    <script src="<?=STATIC_HOST?>/js/dist/app/logistics.js?v=1466663055612"></script>
+    <script src="<?=STATIC_HOST?>/js/dist/app/logistics.js?v=1466667216098"></script>
     <script>
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
