87f6a7092ef2299c6d2f7e092f5ee7a370fa5366
sunchengbin <sunchengbin@weidian.com>
2016-06-24 13:55:04 +0800

    修改bug

--- html/detail.php
+++ html/detail.php
@@ -10,7 +10,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
   <meta name="apple-touch-fullscreen" content="yes"/>
   <meta name="apple-mobile-web-app-capable" content="yes"/>
   <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
-  <link href="<?=STATIC_HOST?>/css/dist/app/item.css?v=1466747526092" rel="stylesheet"/>
+  <link href="<?=STATIC_HOST?>/css/dist/app/item.css?v=1466747679528" rel="stylesheet"/>
   <title>Item detail</title>
     <?php
         include_once( dirname(__FILE__).'/../html/router/util.php' );
@@ -30,9 +30,9 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
         ?>
 </head>
 <body>
-    <script src="<?=STATIC_HOST?>/js/base/require-zepto.js?v=1466747526092"></script>
-    <!--<script src="<?=STATIC_HOST?>/js/base/require-config.js?v=1466747526092"></script>-->
-    <script src="<?=STATIC_HOST?>/js/dist/app/item.js?v=1466747526092"></script>
+    <script src="<?=STATIC_HOST?>/js/base/require-zepto.js?v=1466747679528"></script>
+    <!--<script src="<?=STATIC_HOST?>/js/base/require-config.js?v=1466747679528"></script>-->
+    <script src="<?=STATIC_HOST?>/js/dist/app/item.js?v=1466747679528"></script>
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
