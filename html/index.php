00b16eafda99a5a9ed1225d0e9e71f3c57245c8b
sunchengbin <sunchengbin@weidian.com>
2016-06-24 13:57:10 +0800

    修改bug

--- html/index.php
+++ html/index.php
@@ -34,8 +34,8 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
 </head>
 <body>
   <script src="<?=STATIC_HOST?>/js/base/require-zepto.js?v=1466747679528"></script>
-  <script src="<?=STATIC_HOST?>/js/base/require-config.js?v=1466747679528"></script>
-  <script src="<?=STATIC_HOST?>/js/app/index.js?v=1466747679528"></script>
+  <!--<script src="<?=STATIC_HOST?>/js/base/require-config.js?v=1466747679528"></script>-->
+  <script src="<?=STATIC_HOST?>/js/dist/app/index.js?v=1466747679528"></script>
   <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
