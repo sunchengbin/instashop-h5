e3dc19f1eede19b0464c60c86c74bc5e3cbe9779
sunchengbin <sunchengbin@weidian.com>
2016-06-24 15:37:12 +0800

    修改bug

--- html/orderconfirm.php
+++ html/orderconfirm.php
@@ -37,7 +37,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
       </script>
 </head>
 <body>
-    <script src="<?=STATIC_HOST?>/js/base/require-zepto.js?v=1466747679528"></script>
+    <script src="<?=STATIC_HOST?>/js/base/require-zepto.js?v=1466753823147"></script>
     <!--<script src="<?=STATIC_HOST?>/js/base/require-config.js"></script>-->
     <script src="<?=STATIC_HOST?>/js/dist/app/orderconfirm.js"></script>
     <script>
