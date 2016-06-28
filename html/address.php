35979a00f085eb41d4a0ebb50796405f46c2867d
sunchengbin <sunchengbin@weidian.com>
2016-06-28 11:49:44 +0800

    修改icon提交测试环境

--- html/address.php
+++ html/address.php
@@ -10,14 +10,14 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     <meta http-equiv="cache-control" content="no-cache">
     <meta http-equiv="expect" content="0">
     <?=STATIC_FONT_CSS?>
-    <link href="<?=STATIC_HOST?>/css/dist/app/address.css?v=1467079432132" rel="stylesheet"/>
+    <link href="<?=STATIC_HOST?>/css/dist/app/address.css?v=1467085764720" rel="stylesheet"/>
     <title>Address</title>
 </head>
 <body>
 
-    <script src="<?=STATIC_HOST?>/js/base/require-zepto.js?v=1467079432132"></script>
+    <script src="<?=STATIC_HOST?>/js/base/require-zepto.js?v=1467085764720"></script>
     <!--<script src="<?=STATIC_HOST?>/js/base/require-config.js"></script>-->
-    <script src="<?=STATIC_HOST?>/js/dist/app/address.js?v=1467079432132"></script>
+    <script src="<?=STATIC_HOST?>/js/dist/app/address.js?v=1467085764720"></script>
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
