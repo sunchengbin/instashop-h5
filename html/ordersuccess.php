7fa796bc6219101e75fad9c9422cadf3762883cb
sunchengbin <sunchengbin@weidian.com>
2016-07-06 18:10:45 +0800

    测试环境2.1版本

--- html/ordersuccess.php
+++ html/ordersuccess.php
@@ -12,7 +12,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     <?=STATIC_DNS?>
     <?=STATIC_ICO_CSS?>
     <?=STATIC_FONT_CSS?>
-    <link href="<?=STATIC_HOST?>/css/dist/app/ordersuccess.css?v=1467692192568" rel="stylesheet"/>
+    <link href="<?=STATIC_HOST?>/css/dist/app/ordersuccess.css?v=1467799721645" rel="stylesheet"/>
     <title>Status Pesanan</title>
 </head>
 <body>
--- html/router/common.php
+++ html/router/common.php
@@ -33,7 +33,7 @@ function loadClass($strClassName)
     }
 }
 function getFontCss($url){
-    return '<style>@font-face {font-family: "iconfont";src: url("'.$url.'/css/base/fonts/iconfont.ttf?v=1467290761352") format("truetype"),url("'.$url.'/css/base/fonts/iconfont.svg?v=1467080658#iconfont") format("svg");}</style>';
+    return '<style>@font-face {font-family: "iconfont";src: url("'.$url.'/css/base/fonts/iconfont.ttf?v=1467799737082") format("truetype"),url("'.$url.'/css/base/fonts/iconfont.svg?v=1467799737082#iconfont") format("svg");}</style>';
 }
 function getIco($url){
     return '<link rel="shortcut icon" href="'.$url.'/favicon.ico" type="image/vnd.microsoft.icon"><link rel="icon" href="'.$url.'/favicon.ico" type="image/vnd.microsoft.icon">';
