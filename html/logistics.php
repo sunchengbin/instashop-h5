35366641177f69ce402ce66d7667e01baa1bf289
sunchengbin <sunchengbin@weidian.com>
2016-07-07 10:08:52 +0800

    测试环境2.1版本

--- html/logistics.php
+++ html/logistics.php
@@ -13,7 +13,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     <?=STATIC_DNS?>
     <?=STATIC_ICO_CSS?>
     <?=STATIC_FONT_CSS?>
-    <link href="<?=STATIC_HOST?>/css/dist/app/logistics.css?v=1467799721645" rel="stylesheet"/>
+    <link href="<?=STATIC_HOST?>/css/dist/app/logistics.css?v=1467857321400" rel="stylesheet"/>
     <title>Detail pengiriman</title>
     <script>
         <?php
@@ -38,9 +38,9 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
       </script>
 </head>
 <body>
-    <script src="<?=STATIC_HOST?>/js/base/require-zepto.js?v=1467799721645"></script>
+    <script src="<?=STATIC_HOST?>/js/base/require-zepto.js?v=1467857321400"></script>
     <!--<script src="../js/base/require-config.js"></script>-->
-    <script src="<?=STATIC_HOST?>/js/dist/app/logistics.js?v=1467799721645"></script>
+    <script src="<?=STATIC_HOST?>/js/dist/app/logistics.js?v=1467857321400"></script>
     <script>
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
