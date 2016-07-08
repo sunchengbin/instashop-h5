0afc14174b02751b18af77ac06f7d04cb22714d5
sunchengbin <sunchengbin@weidian.com>
2016-07-08 11:12:19 +0800

    修改bug

--- html/orderdetail.php
+++ html/orderdetail.php
@@ -12,7 +12,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     <?=STATIC_DNS?>
     <?=STATIC_ICO_CSS?>
     <?=STATIC_FONT_CSS?>
-    <link href="<?=STATIC_HOST?>/css/dist/app/orderdetail.css?v=1467857321400" rel="stylesheet"/>
+    <link href="<?=STATIC_HOST?>/css/dist/app/orderdetail.css?v=1467947527244" rel="stylesheet"/>
     <title>order detail</title>
     <script>
         <?php
@@ -32,9 +32,9 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     </script>
 </head>
 <body>
-    <script src="<?=STATIC_HOST?>/js/base/require-zepto.js?v=1467857321400"></script>
+    <script src="<?=STATIC_HOST?>/js/base/require-zepto.js?v=1467947527244"></script>
     <!--<script src="../js/base/require-config.js"></script>-->
-    <script src="<?=STATIC_HOST?>/js/dist/app/orderdetail.js?v=1467857321400"></script>
+    <script src="<?=STATIC_HOST?>/js/dist/app/orderdetail.js?v=1467947527244"></script>
     <script>
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
