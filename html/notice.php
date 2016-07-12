24249e73fb75c54c1f564cbda99c33e2e0b597c7
sunchengbin <sunchengbin@weidian.com>
2016-07-12 16:17:39 +0800

    notice页面

---
+++ html/notice.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<?php
+include_once( dirname(__FILE__).'/../html/router/common.php');
+?>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no"/>
+  <meta content="telephone=no" name="format-detection"/>
+  <meta name="apple-touch-fullscreen" content="yes"/>
+  <meta name="apple-mobile-web-app-capable" content="yes"/>
+  <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
+  <?=STATIC_ICO_CSS?>
+  <style>
+  *{
+    padding:0;
+    margin:0;
+  }
+  html { font-size: 62.5%; }
+  @media only screen and (min-width: 374px) {
+      html { font-size: 72%!important }
+  }
+  @media only screen and (min-width: 413px) {
+      html { font-size: 80%!important }
+  }
+  @media only screen and (min-width: 481px) {
+      html { font-size: 94%!important }
+  }
+  @media only screen and (min-width: 561px) {
+      html { font-size: 109%!important }
+  }
+  @media only screen and (min-width: 641px) {
+      html { font-size: 125%!important }
+  }
+  @media(orientation: landscape) and (min-width: 481px) {
+      html { font-size: 62.5% !important; }
+  }
+  body {
+      font: 1.2rem/1.5 'Myriad Set Pro', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+      color:#333;
+  }
+  section{
+    font-size:1.4rem;
+    color:#666;
+    padding: 1rem 2rem;
+  }
+  .header-nav{
+      text-align:center;
+      padding: 0 1rem;
+      height: 4.6rem;
+      line-height: 4.6rem;
+      background-color: #43CB9C;
+      font-size: 1.6rem;
+      color: #fff;
+  }
+  </style>
+  <title>notice</title>
+</head>
+<body>
+    <?php
+        include_once( dirname(__FILE__).'/../html/router/util.php' );
+        $params = [
+            'action' => 'index',
+            'page_size' => 10,
+            'last_id' => '',
+            'json' => '0'
+        ];
+        $notice_id = $_REQUEST['notice_id'];
+        if (!$notice_id) {
+            $ss = split('\/', $_SERVER['REQUEST_URI']);
+            $notice_id = end($ss);
+        }
+        $path = 'v1/notice/'.$notice_id;
+        $ret = get_init_php_data($path, '');
+        $json = json_decode($ret, true);
+        echo '<p class="header-nav">'.$json['notice']['title'].'</p>';
+        echo '<section>'.$json['notice']['content'].'</section>';
+    ?>
+  <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-78448705-7', 'auto');
+      ga('send', 'pageview');
+
+  </script>
+</body>
+</html>
