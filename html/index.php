7cc3688b291caf7806b822a67289d81157f5f958
sunchengbin <sunchengbin@weidian.com>
2016-07-27 16:33:12 +0800

    快速下单线上修改文案

--- html/index.php
+++ html/index.php
@@ -13,7 +13,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
   <?=STATIC_DNS?>
   <?=STATIC_ICO_CSS?>
   <?=STATIC_FONT_CSS?>
-  <link href="<?=STATIC_HOST?>/css/dist/app/shop_index.css?v=1469594573578" rel="stylesheet"/>
+  <link href="<?=STATIC_HOST?>/css/dist/app/shop_index.css?v=1469608359704" rel="stylesheet"/>
     <?php
         include_once( dirname(__FILE__).'/../html/router/util.php' );
         $params = [
@@ -40,7 +40,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
 <body>
   <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
   <!--<script src="<?=STATIC_HOST?>/js/base/require-config.js"></script>-->
-  <script src="<?=STATIC_HOST?>/js/dist/app/index.js?v=1469594573578"></script>
+  <script src="<?=STATIC_HOST?>/js/dist/app/index.js?v=1469608359704"></script>
   <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
