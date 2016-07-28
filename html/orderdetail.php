e4cbbf23cd6dbc3175d95349524fd6189e120059
sunchengbin <sunchengbin@weidian.com>
2016-07-28 18:50:29 +0800

    分类

--- html/orderdetail.php
+++ html/orderdetail.php
@@ -12,7 +12,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     <?=STATIC_DNS?>
     <?=STATIC_ICO_CSS?>
     <?=STATIC_FONT_CSS?>
-    <link href="<?=STATIC_HOST?>/css/dist/app/orderdetail.css?v=1469700744892" rel="stylesheet"/>
+    <link href="<?=STATIC_HOST?>/css/dist/app/orderdetail.css?v=1469703023691" rel="stylesheet"/>
     <title>Detail Pesanan</title>
     <script>
         <?php
@@ -34,7 +34,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
 <body>
     <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
     <!--<script src="../js/base/require-config.js"></script>-->
-    <script src="<?=STATIC_HOST?>/js/dist/app/orderdetail.js?v=1469700744892"></script>
+    <script src="<?=STATIC_HOST?>/js/dist/app/orderdetail.js?v=1469703023691"></script>
     <script>
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
