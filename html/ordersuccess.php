5fc358d3342434e232fb25321295b81494487c76
sunchengbin <sunchengbin@weidian.com>
2016-07-29 09:40:33 +0800

    分类进入商品详情页的回退按钮跳转地址修改

--- html/ordersuccess.php
+++ html/ordersuccess.php
@@ -12,14 +12,14 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     <?=STATIC_DNS?>
     <?=STATIC_ICO_CSS?>
     <?=STATIC_FONT_CSS?>
-    <link href="<?=STATIC_HOST?>/css/dist/app/ordersuccess.css?v=1469703023691" rel="stylesheet"/>
+    <link href="<?=STATIC_HOST?>/css/dist/app/ordersuccess.css?v=1469756413734" rel="stylesheet"/>
     <title>Informasi Pembayaran</title>
 </head>
 <body>
 
     <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
     <!--<script src="<?=STATIC_HOST?>/js/base/require-config.js"></script>-->
-    <script src="<?=STATIC_HOST?>/js/dist/app/ordersuccess.js?v=1469703023691"></script>
+    <script src="<?=STATIC_HOST?>/js/dist/app/ordersuccess.js?v=1469756413734"></script>
     <script>
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
