55f832605cdb55f969cb0787b92333da87d2b3b2
sunchengbin <sunchengbin@weidian.com>
2016-08-09 12:03:54 +0800

    测试环境,调试地址滚动条bug

--- html/usertest.php
+++ html/usertest.php
@@ -117,6 +117,6 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     <section class="j_show_info">
         <p>loading...</p>
     </section>
-    <script src="<?=STATIC_HOST?>/js/app/usertest.js?v=1470654301890"></script>
+    <script src="<?=STATIC_HOST?>/js/app/usertest.js?v=1470715428324"></script>
 </body>
 </html>
