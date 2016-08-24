d153779dec05cb33c5ab57244e7a58fc424150b4
sunchengbin <sunchengbin@weidian.com>
2016-08-24 14:21:38 +0800

    master,修改分类翻页bug

--- static/js/app/sort.js
+++ static/js/app/sort.js
@@ -60,13 +60,14 @@ require(['lang','lazyload','hbs','text!views/app/sort.hbs','ajax','config','base
             if(init_data.code == 200){
                 var _list = init_data.item_list.list,
                     _last_id = _list.length?_list[(_list.length-1)].id:null,
+                    _tag_id = init_data.tag.id,
                     _this = this,
                     getData = _last_id?true:false,
                     reqData = {
                         edata : {
                             action : 'tag',
                             page_size : 10,
-                            tag_id : Base.others.getUrlPrem('sort_id'),
+                            tag_id : _tag_id,
                             last_id : _last_id
                         }
                     };
@@ -76,7 +77,7 @@ require(['lang','lazyload','hbs','text!views/app/sort.hbs','ajax','config','base
                         _bh = $(document).height();
                     if ((_st + _wh > _bh - 200) && getData) {
                         getData = false;
-                        Ajax.getJsonp(Config.host.actionUrl+Config.actions.shopList+Base.others.getUrlPrem('seller_id')+'/items?param='+JSON.stringify(reqData),function(obj){
+                        Ajax.getJsonp(Config.host.actionUrl+Config.actions.shopList+init_data.tag.seller_id+'/items?param='+JSON.stringify(reqData),function(obj){
                             if(obj.code == 200){
                                 var _list = obj.item_list.list,
                                     _last_id = _list.length?_list[(_list.length-1)].id:null;
@@ -84,7 +85,7 @@ require(['lang','lazyload','hbs','text!views/app/sort.hbs','ajax','config','base
                                     edata : {
                                         action : 'tag',
                                         page_size : 10,
-                                        tag_id : Base.others.getUrlPrem('sort_id'),
+                                        tag_id : _tag_id,
                                         last_id : _last_id,
                                         havestock : 1
                                     }
