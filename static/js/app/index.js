62719efd3e67bc883b9de6a8fe09d9fa48aac38d
sunchengbin <sunchengbin@weidian.com>
2016-09-06 17:49:01 +0800

    2.8测试环境修改item页面的url

--- static/js/app/index.js
+++ static/js/app/index.js
@@ -8,7 +8,7 @@ require(['lang','lazyload','hbs','text!views/app/index.hbs','ajax','config','bas
             var IndexHtm = '<div>'+Lang.H5_LOADING+'</div>',
                 _this = this;
             _this.sortTimes = 0;
-            if(init_data && init_data == 200){
+            if(init_data && init_data.code == 200){
                 Common.initShopInfo(init_data);
                 var _tag_list = _this.getTags(init_data.item_list.list),
                     _init_data = {
@@ -323,8 +323,9 @@ require(['lang','lazyload','hbs','text!views/app/index.hbs','ajax','config','bas
                 _webplog = !Base.others.webpLog(),
                 i = 0;
             for (i; i < items.length;i++) {
-                var _time = _this.discountTime(items[i].discount.now_time,items[i].discount.end_time);
-                out += '<li><a class="item-info j_item_info" data-url="'+(Config.host.host+'detail/'+items[i].id+(_webplog?'?webpLog=1':''))+'" href="javascript:;">'
+                var _time = _this.discountTime(items[i].discount.now_time,items[i].discount.end_time),
+                    _url = Base.others.isCustomHost()?Config.host.host+items[i].id:Config.host.host+'detail/'+items[i].id;
+                out += '<li><a class="item-info j_item_info" data-url="'+(_url+(_webplog?'?webpLog=1':''))+'" href="javascript:;">'
                     +'<div class="lazy" data-img="'+Base.others.cutImg(items[i].img,160)+'">';
                 if(items[i].is_discount){
                     out +='<span>-'+items[i].discount.value+'%</span>';
