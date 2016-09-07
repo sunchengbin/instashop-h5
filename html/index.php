f63e1eeea1886a8de6a147c0e0c1cde9bfd053c5
sunchengbin <sunchengbin@weidian.com>
2016-09-07 12:03:55 +0800

    修改h5商品详情页样式

--- html/index.php
+++ html/index.php
@@ -13,7 +13,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
   <?=STATIC_DNS?>
   <?=STATIC_ICO_CSS?>
   <?=STATIC_FONT_CSS?>
-  <link href="<?=STATIC_HOST?>/css/dist/app/shop_index.css?v=1473066842680" rel="stylesheet"/>
+  <link href="<?=STATIC_HOST?>/css/dist/app/shop_index.css?v=1473220700197" rel="stylesheet"/>
     <?php
         include_once( dirname(__FILE__).'/../html/router/util.php' );
         $params = [
@@ -47,7 +47,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
   <div class="j_php_loding">Memuat...</div>
   <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
   <!--<script src="<?=STATIC_HOST?>/js/base/require-config.js"></script>-->
-  <script src="<?=STATIC_HOST?>/js/dist/app/index.js?v=1473066842680"></script>
+  <script src="<?=STATIC_HOST?>/js/dist/app/index.js?v=1473220700197"></script>
   <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
