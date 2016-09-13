db2d72bf2f501163a991b74cb0b50afd609ba97c
sunchengbin <sunchengbin@weidian.com>
2016-09-13 14:15:59 +0800

    master合并了2.8和3.0

--- html/logistics.php
+++ html/logistics.php
@@ -13,7 +13,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     <?=STATIC_DNS?>
     <?=STATIC_ICO_CSS?>
     <?=STATIC_FONT_CSS?>
-    <link href="<?=STATIC_HOST?>/css/dist/app/logistics.css?v=1473691724995" rel="stylesheet"/>
+    <link href="<?=STATIC_HOST?>/css/dist/app/logistics.css?v=1473746806276" rel="stylesheet"/>
     <title>Detail pengiriman</title>
     <script>
         <?php
@@ -40,7 +40,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
 <body>
     <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
     <!--<script src="../js/base/require-config.js"></script>-->
-    <script src="<?=STATIC_HOST?>/js/dist/app/logistics.js?v=1473691724995"></script>
+    <script src="<?=STATIC_HOST?>/js/dist/app/logistics.js?v=1473746806276"></script>
     <script>
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
