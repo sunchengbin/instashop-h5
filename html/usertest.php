840dc7edaf246b674f10db05e0c64582d1b3a079
sunchengbin <sunchengbin@weidian.com>
2016-09-18 16:05:54 +0800

    修改address页面bug

--- html/usertest.php
+++ html/usertest.php
@@ -145,6 +145,6 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     </section>
     <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
     <script src="<?=STATIC_HOST?>/js/base/require-config.js"></script>
-    <script src="<?=STATIC_HOST?>/js/app/usertest.js?v=1473841827076"></script>
+    <script src="<?=STATIC_HOST?>/js/app/usertest.js?v=1474185946396"></script>
 </body>
 </html>
