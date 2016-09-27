2547dfe3c1d87b87bc2dbca9868575fb6b2bd0ae
sunchengbin <sunchengbin@weidian.com>
2016-09-27 15:05:27 +0800

    3.1上线

--- html/usertest.php
+++ html/usertest.php
@@ -145,6 +145,6 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     </section>
     <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
     <script src="<?=STATIC_HOST?>/js/base/require-config.js"></script>
-    <script src="<?=STATIC_HOST?>/js/app/usertest.js?v=1474856699932"></script>
+    <script src="<?=STATIC_HOST?>/js/app/usertest.js?v=1474959750895"></script>
 </body>
 </html>
