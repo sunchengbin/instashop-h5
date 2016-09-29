cc5653dee5bf938dabebdf92a8aed7e63a735e4e
sunchengbin <sunchengbin@weidian.com>
2016-09-29 17:54:01 +0800

    修改轮播图在pc端的bug

---
+++ html/404.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<?php
+include_once( dirname(__FILE__).'/../html/router/common.php');
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1, minimum-scale=1.0, maximum-scale=1.0,user-scalable=no">
+    <meta http-equiv="pragma" content="no-cache">
+    <meta http-equiv="cache-control" content="no-cache">
+    <meta http-equiv="expect" content="0">
+    <meta name="format-detection" content="telephone=no" />
+    <?=STATIC_DNS?>
+    <?=STATIC_ICO_CSS?>
+    <?=STATIC_FONT_CSS?>
+    <title>404</title>
+</head>
+<body>
+    <div>
+
+    </div>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-78448705-7', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+</body>
+</html>
