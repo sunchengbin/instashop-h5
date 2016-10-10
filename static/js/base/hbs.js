ba86348abf407edf773724bf36ca313b7fd335b3
sunchengbin <sunchengbin@weidian.com>
2016-10-10 15:23:07 +0800

    修改首页cssbug,订单确认页面选择物流插件bug

--- static/js/base/hbs.js
+++ static/js/base/hbs.js
@@ -536,15 +536,17 @@ define(['handlebars','base','config','lang'], function(HBS,Base,Config,Lang) {
     HBS.registerHelper('logistics', function(data) {
         var _htm = '';
         for(var item in data){
-            _htm += '<ul class="logistics-list">';
-            for(var i in data[item]){
-                var _cost_day = data[item][i].cost_days?'('+data[item][i].cost_days+Lang.H5_DAYS+')':'';
-                _htm += '<li class="j_logistics_li" data-company="'+item+'"  data-level="'+(data[item][i].level?data[item][i].level:'')+'" data-id="'+data[item][i].id+'">'
-                    +'<i class="icon iconfont check-btn icon-radio-font" data-company="'+item+'" data-price="'+data[item][i].price+'"  data-level="'+(data[item][i].level?data[item][i].level:'')+'" data-id="'+data[item][i].id+'"></i>'
-                    +item+' '+data[item][i].level+_cost_day+': Rp '+Base.others.priceFormat(data[item][i].price)
-                    +'</li>';
+            if(data[item].length > 0){
+                _htm += '<ul class="logistics-list">';
+                for(var i in data[item]){
+                    var _cost_day = data[item][i].cost_days?'('+data[item][i].cost_days+Lang.H5_DAYS+')':'';
+                    _htm += '<li class="j_logistics_li" data-company="'+item+'"  data-level="'+(data[item][i].level?data[item][i].level:'')+'" data-id="'+data[item][i].id+'">'
+                        +'<i class="icon iconfont check-btn icon-radio-font" data-company="'+item+'" data-price="'+data[item][i].price+'"  data-level="'+(data[item][i].level?data[item][i].level:'')+'" data-id="'+data[item][i].id+'"></i>'
+                        +item+' '+data[item][i].level+_cost_day+': Rp '+Base.others.priceFormat(data[item][i].price)
+                        +'</li>';
+                }
+                _htm += '</ul>';
             }
-            _htm += '</ul>';
         }
         return _htm;
     });
