5f47cb6ed20947bd9bc3ad9fa15f6c63b57ce3b3
sunchengbin <sunchengbin@weidian.com>
2016-10-10 11:22:41 +0800

    ui修改,商品订单店铺不存在判断.

--- static/js/moudle/config/config.js
+++ static/js/moudle/config/config.js
@@ -20,25 +20,25 @@ define([],function(){
             //phpHost : 'https://api-test.instashop.co.id/instashop/instashop/im/',
             //hrefHost : 'https://m-test.instashop.co.id/html'
             //线上
-            //hostUrl : URL_HTTP_TYPE+'//'+URL_HOST_NAME+'/html/',//内部router的路径
-            //host : URL_HTTP_TYPE+'//'+URL_HOST_NAME+'/',
-            //imgUrl : URL_HTTP_TYPE+'//static.instashop.co.id/images',
-            //hrefUrl : URL_HTTP_TYPE+'//'+URL_HOST_NAME+'/html/',
-            //hrefHost : URL_HTTP_TYPE+'//'+URL_HOST_NAME+'/html',
-            //actionUrl : 'https://apip.instashop.co.id/instashop/',
-            //imHost : 'http://218.213.86.206',
-            //phpHost : 'http://api.instashop.co.id/instashop/instashop/im/',
-            //imUserNameHost : 'http://218.213.86.206:2040/'//存储im用户名字
-            //本地开发环境
             hostUrl : URL_HTTP_TYPE+'//'+URL_HOST_NAME+'/html/',//内部router的路径
             host : URL_HTTP_TYPE+'//'+URL_HOST_NAME+'/',
-            imgUrl : URL_HTTP_TYPE+'//static-test.instashop.co.id/images',
+            imgUrl : URL_HTTP_TYPE+'//static.instashop.co.id/images',
             hrefUrl : URL_HTTP_TYPE+'//'+URL_HOST_NAME+'/html/',
             hrefHost : URL_HTTP_TYPE+'//'+URL_HOST_NAME+'/html',
-            actionUrl : 'https://apip-test.instashop.co.id/instashop/',
-            imHost : 'http://10.5.15.10:2000',
-            imUserNameHost : 'http://10.5.15.10:2040/',//存储im用户名字
-            phpHost : 'http://10.5.15.10:8888/instashop/instashop/im/'
+            actionUrl : 'https://apip.instashop.co.id/instashop/',
+            imHost : 'http://218.213.86.206',
+            phpHost : 'http://api.instashop.co.id/instashop/instashop/im/',
+            imUserNameHost : 'http://218.213.86.206:2040/'//存储im用户名字
+            //本地开发环境
+            //hostUrl : URL_HTTP_TYPE+'//'+URL_HOST_NAME+'/html/',//内部router的路径
+            //host : URL_HTTP_TYPE+'//'+URL_HOST_NAME+'/',
+            //imgUrl : URL_HTTP_TYPE+'//static-test.instashop.co.id/images',
+            //hrefUrl : URL_HTTP_TYPE+'//'+URL_HOST_NAME+'/html/',
+            //hrefHost : URL_HTTP_TYPE+'//'+URL_HOST_NAME+'/html',
+            //actionUrl : 'https://apip-test.instashop.co.id/instashop/',
+            //imHost : 'http://10.5.15.10:2000',
+            //imUserNameHost : 'http://10.5.15.10:2040/',//存储im用户名字
+            //phpHost : 'http://10.5.15.10:8888/instashop/instashop/im/'
         },
         linkUrl : {
             index : 's/',
