fe8670d2c24d2c472cf3c35e942e3b861bc8e01d
sunchengbin <sunchengbin@weidian.com>
2016-10-12 14:27:59 +0800

    修改图片链接为协议访问方式，账户查询页面账户为.

--- html/quickcarts.php
+++ html/quickcarts.php
@@ -14,7 +14,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
   <?=STATIC_ICO_CSS?>
   <?=STATIC_FONT_CSS?>
   <title>Alamat Pengiriman</title>
-  <link href="<?=STATIC_HOST?>/css/dist/app/quickcarts.css?v=1476237907576" rel="stylesheet"/>
+  <link href="<?=STATIC_HOST?>/css/dist/app/quickcarts.css?v=1476253596487" rel="stylesheet"/>
     <?php
         include_once( dirname(__FILE__).'/../html/router/util.php' );
         $cart_id = $_REQUEST['cart_id'];
@@ -37,7 +37,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     <div class="j_php_loding">Memuat...</div>
   <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
   <!--<script src="<?=STATIC_HOST?>/js/base/require-config.js"></script>-->
-  <script src="<?=STATIC_HOST?>/js/dist/app/quickcarts.js?v=1476237907576"></script>
+  <script src="<?=STATIC_HOST?>/js/dist/app/quickcarts.js?v=1476253596487"></script>
   <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
