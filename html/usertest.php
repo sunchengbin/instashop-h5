f59194d7753a7a33195c240f3f6711ff2f656958
sunchengbin <sunchengbin@weidian.com>
2016-10-20 15:35:19 +0800

    修改线上商品详情页搬家待上架商品置为不存在

--- html/usertest.php
+++ html/usertest.php
@@ -145,6 +145,6 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     </section>
     <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
     <script src="<?=STATIC_HOST?>/js/base/require-config.js"></script>
-    <script src="<?=STATIC_HOST?>/js/app/usertest.js?v=1476787231648"></script>
+    <script src="<?=STATIC_HOST?>/js/app/usertest.js?v=1476948624691"></script>
 </body>
 </html>
