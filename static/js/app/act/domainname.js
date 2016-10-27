c0d40b830e2baba020f38d2ef0d872dcc5ecf223
sunchengbin <sunchengbin@weidian.com>
2016-10-27 17:18:01 +0800

    3.3活动页面

--- static/js/app/act/domainname.js
+++ static/js/app/act/domainname.js
@@ -17,6 +17,7 @@ require(['config','ajax','slide','dialog'],function(Config,Ajax,Slide,Dialog){
                     if(typeof obj == 'string'){
                         obj = JSON.parse(obj);
                     }
+                    alert(typeof obj);
                     alert(obj.length);
                     alert(obj.seller_id);
                     _this.user_info = obj;
