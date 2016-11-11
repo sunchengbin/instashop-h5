115ba367825fa51f1c0c08018fec69fe90c20515
lanchenghao <lanchenghao@weidian.com>
2016-11-11 14:49:40 +0800

    PC端商品详情页优化需求
    商品详情页在PC端支持点击“左、右”按钮浏览各张商品图片（目前只能看一张），商品图限定尺寸等比缩放而不是截掉

--- static/js/moudle/ui/viewer/viewer.js
+++ static/js/moudle/ui/viewer/viewer.js
@@ -2,7 +2,7 @@
  * Created by sunchengbin on 16/10/19.
  * 查看大图
  */
-define(['base','slide'],function(Base,Slide){
+define(['base','slide','dialog'],function(Base,Slide,Dialog){
     var VIEWER = function(opts){
         var _this = this;
         _this.config = $.extend({
@@ -12,6 +12,19 @@ define(['base','slide'],function(Base,Slide){
             images : null
         },opts);
         _this.isSlide = opts.images.length>1;
+        _this.whichPlatform = (function (win) {
+            var _is_pc = false;
+            var _is_phone = false;
+            if (/Android|webOS|iPhone|iPod|BlackBerry|iPad|SymbianOS|Windows Phone/i.test(win.navigator.userAgent)) {
+                _is_phone = true;
+            } else {
+                _is_pc = true;
+            }
+            return {
+                isPc: _is_pc,
+                isPhone: _is_phone
+            }
+        })(window);
     };
     VIEWER.prototype = {
         init : function(){
@@ -35,22 +48,27 @@ define(['base','slide'],function(Base,Slide){
             var _this = this,
                 _imgs = _this.config.images;
             if($('.j_viewer_box').length == 0){
-                var _html = '';
+                var $viewerBox = $('<div class="viewer-box j_viewer_box"></div>');
+                _this.$viewerBoxUl = $('<ul class="j_viewer_ul clearfix"></ul>');
+                $viewerBox.append(_this.$viewerBoxUl);
                 if(_this.isSlide){
-                    _html = '<div class="viewer-box j_viewer_box"><ul class="j_viewer_ul clearfix">';
                     for(var i = 0;i < _imgs.length;i++){
-                        _html += '<li><div><img src="'+_this.getImageUrl(_imgs[i])+'"></div></li>';
+                        if(i==index){
+                            _this.$viewerBoxUl.append($('<li data-src="'+_this.getImageUrl(_imgs[i])+'"><div><img src="'+_this.getImageUrl(_imgs[i])+'"></div></li>'))
+                        }else{
+                            _this.$viewerBoxUl.append($('<li data-src="'+_this.getImageUrl(_imgs[i])+'"><div><img src=""></div></li>'))
+                        }
                     }
-                    _html += '</ul></div>';
                 }else{
-                    _html = '<div class="viewer-box j_viewer_box"><ul class="j_viewer_ul clearfix"><li><div><img src="'+_this.getImageUrl(url)+'"></div></li></ul></div>';
+                    _this.$viewerBoxUl.append('<li><div><img src="'+_this.getImageUrl(url)+'"></div></li>')
                 }
-                $('body').append(_html);
+                $('body').append($viewerBox);
                 $('.j_viewer_ul li').css({
                     width : $(window).width()
                 });
                 $('.j_viewer_ul li div').css({
-                    height : $(window).height()
+                    height : $(window).height(),
+                    lineHeight:$(window).height()+"px"
                 });
             }
             $('.j_viewer_box').css('z-index',Base.others.zIndex);
@@ -60,29 +78,59 @@ define(['base','slide'],function(Base,Slide){
             return url.split('?')[0]+'?w=750';
         },
         loadImg : function(url,callback){
+            var _this = this;
             var _img = new Image();
             _img.src = url;
             _img.onload = function(){
-                callback && callback();
+                callback && callback(_img);
             };
             _img.onerror = function(){
-
+                _this._loading.remove();
+            }
+        },
+        fixedImageSize:function(_viewerImages,_img,index){
+            var _curImg = $(_viewerImages[index]).find("img");
+            _curImg.css({
+                height:"100%",
+                width:"inherit"
+            })
+            if(_img.width<_img.height){
+                _curImg.css({
+                    background:"#000"
+                })
             }
         },
         show : function(url,index){
             var _this = this;
             _this.createViewer(url,index);
-            _this.loadImg(url,function(){
-                $('.j_viewer_box').show();
+            _this._loading = Dialog.loading({
+                width:100,
+                is_cover:false
+            })
+            var _viewerImages = [];
+            var _viewerLis = _this.$viewerBoxUl.children();
+            for(var i=0,len=_viewerLis.length;i<len;i++){
+                _viewerImages.push(_viewerLis[i]);
+            }
+            $('.j_viewer_box').show();
+            _this.loadImg(url,function(_img){
+                if(_this.whichPlatform.isPc){
+                    _this.fixedImageSize(_viewerImages,_img,index)
+                }
+                _this._loading.remove();
+
                 Slide.createNew({
                     dom: document.querySelector('.j_viewer_ul'),
                     needTab: true,
                     auto : false,
+                    viewerImageLis:_viewerImages,
                     curPage : index+1
                 });
             });
         },
         hide : function(){
+            var _this = this;
+            if(_this._loading)_this._loading.remove();
             $('.j_viewer_box').remove();
         }
     };
