b2e488b988b57f0b2fac7ac7da84f5ed04b7b4aa
sunchengbin <sunchengbin@weidian.com>
2016-11-11 10:06:09 +0800

    3.5测试用例上测试环境

---
+++ html/editmodel.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<?php
+include_once( dirname(__FILE__).'/../html/router/common.php');
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1, minimum-scale=1.0, maximum-scale=1.0,user-scalable=no">
+    <meta http-equiv="pragma" content="no-cache">
+    <meta http-equiv="cache-control" content="no-cache">
+    <meta http-equiv="expect" content="0">
+    <meta name="format-detection" content="telephone=no" />
+    <?=STATIC_DNS?>
+    <?=STATIC_ICO_CSS?>
+    <?=STATIC_FONT_CSS?>
+    <link href="<?=STATIC_HOST?>/css/dist/app/orderconfirm.css?v=1478407361226" rel="stylesheet"/>
+    <title>店铺装修</title>
+    <script>
+        <?php
+        include_once( dirname(__FILE__).'/../html/router/util.php' );
+        error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
+        ini_set('display_errors', 0);
+
+        $seller_id = $_REQUEST['seller_id'];
+        $addr = $_REQUEST['addr'];
+        $items = json_decode($_REQUEST['items'],true);
+        $new_addr = $_REQUEST['new_addr'];
+        if ($new_addr) {
+            $addr = $new_addr;
+        }
+        $params = [
+            'action' => 'express_fee',
+            'shop_id' => $seller_id,
+            'items' => $items,
+            'receive_addr' => urlencode($addr)
+        ];
+        $path = 'v1/expresses';
+        ?>
+        var api_url = '<?php echo check_api($path, $params); ?>';
+
+        var express_data = JSON.parse(<?php echo get_init_data($path, $params); ?>);
+
+      </script>
+      <style>
+            button{
+               display: block;
+               width: 200px;
+               height: 40px;
+               line-height: 40px;
+               background-color: #43CB9C;
+               margin-top: 10px;
+               color: #fff;
+               font-size: 16px;
+               border-radius: 2px;
+            }
+      </style>
+</head>
+<body>
+    <button class="j_insert_model">insert_model</button>
+    <button class="j_edit_signage">edit_signage</button>
+    <button class="j_edit_model">edit_model</button>
+    <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
+    <script src="<?=STATIC_HOST?>/js/base/require-config.js"></script>
+    <script src="<?=STATIC_HOST?>/js/app/editmodel.js?v=1478407361226"></script>
+    <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-78448705-7', 'auto');
+          ga('send', 'pageview');
+
+      </script>
+</body>
+</html>
---
+++ static/js/app/editmodel.js
@@ -0,0 +1,120 @@
+/**
+ * Created by sunchengbin on 2016/11/10.
+ * 店铺装修首页
+ */
+require(['base','insjs','fastclick','config'],function(Base,Insjs,FastClick,Config){
+    var EditModel = {
+        init : function(){
+            var _this = this;
+            //_this.initHtml(init_data);
+            Insjs.WebOnReady(function(bridge){
+                _this.handelFn(bridge);
+            },function(){
+                _this.handelFn();
+            });
+        },
+        handelFn : function(bridge){
+            var _this = this;
+            FastClick.attach(document.body);
+            $('body').on('click','.j_insert_model',function(){
+                var _dom = $(this),
+                    _index = $('.j_insert_model').index(_dom);
+                console.log(_index);
+                var _param = {
+                    param:{
+                        type:'create_model',
+                        param:{
+                            index : _index,
+                            url : Config.host.hrefUrl+'modeltype.php?index='+_index
+                        }
+                    }
+                };
+                bridge.callHandler('insSocket',_param, function(response) {
+                      return null;
+                });
+            });
+            $('body').on('click','.j_edit_model',function(){
+                var _dom = $(this),
+                    _index = $('.j_edit_model').index(_dom);
+                console.log(_index);
+                var _param = {
+                    param:{
+                        type:'edit_model',
+                        param:{
+                            index : _index,
+                            data  : {
+                                index : _index,
+                                data : {
+                                    type:'staticBanner',
+                                    data: [{img:'',link_url:''}]
+                                }
+                            }
+                        }
+                    }
+                };
+                bridge.callHandler('insSocket',_param, function(response) {
+                    return null;
+                });
+            });
+            $('body').on('click','.j_del_model',function(){
+
+            });
+            $('body').on('click','.j_moveup_model',function(){
+
+            });
+            $('body').on('click','.j_edit_signage',function(){
+                var _param = {
+                    param:{
+                        type:'edit_signage',
+                        param:{
+                            index : 0,
+                            data  : {
+                                img : ''
+                            }
+                        }
+                    }
+                };
+                bridge.callHandler('insSocket',_param, function(response) {
+                    return null;
+                });
+            });
+        },
+        registerFn : function(bridge){
+            bridge.registerHandler('registerSocket',data, function(responseCallback) {
+                alert(data);
+                responseCallback(responseData);
+            });
+        },
+        initHtml : function(){
+
+        },
+        createModelHtm : function(){
+
+        },
+        staticBannerHtm : function(){
+
+        },
+        rotateBannerHtm : function(){
+
+        },
+        twoListBannerHtm : function(){
+
+        },
+        imgNavigationHtm : function(){
+
+        },
+        textNavigationHtm : function(){
+
+        },
+        twoLiItemsHtm : function(){
+
+        },
+        bigImgItem : function(){
+
+        },
+        listItems : function(){
+
+        }
+    };
+    EditModel.init();
+})
