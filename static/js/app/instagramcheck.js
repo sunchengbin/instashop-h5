67ef02220d3f394d93b8a1ab38c117c94d29b111
lanchenghao <lanchenghao@weidian.com>
2016-11-14 16:19:13 +0800

    注册按钮

--- static/js/app/instagramcheck.js
+++ static/js/app/instagramcheck.js
@@ -4,15 +4,19 @@
 require(['hbs', 'text!views/app/instagramcheck.hbs', 'dialog', 'ajax', 'config', 'base', 'lang', 'fastclick','common'], function (Hbs, Checkhtm, Dialog, Ajax, Config, Base, Lang, Fastclick,Common) {
     var Instagramcheck = {
         init: function () {
+            var ctx = this;
             var _htm = Hbs.compile(Checkhtm)({
                 lang: Lang
             });
             //截取url参数 Common.getQueryParam(key)
             $('body').prepend(_htm);
+            document.querySelector(".j_submit_check").addEventListener("click",function(){
+                ctx.subData();
+            })
         },
         subData: function () {
             var _that = this;
-            var _instagram_name = $().trim($(".j_instagram_name").val())||"";
+            var _instagram_name = $.trim($(".j_instagram_name").val())||"";
             var _reqData = {
                 frm: "1",
                 wduss: Common.getQueryParam("wduss"),
@@ -21,6 +25,7 @@ require(['hbs', 'text!views/app/instagramcheck.hbs', 'dialog', 'ajax', 'config',
                 item_count: "0",
                 action: "import"
             }
+            console.info(_reqData);
             Ajax.postJsonp({
                 url: Config.actions.instagramcheck,
                 data: {param: JSON.stringify(_reqData)},
@@ -28,7 +33,7 @@ require(['hbs', 'text!views/app/instagramcheck.hbs', 'dialog', 'ajax', 'config',
                 success: function (obj) {
                     if (obj.code == 200) {
                     } else {
-                        window.location.href = "instashop://app/instagram_move?id=bear";
+                        window.location.href = "instashop://app/instagram_move?id="+_instagram_name;
                     }
                 },
                 error: function (error) {
