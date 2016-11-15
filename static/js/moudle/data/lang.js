8cfdf0a8402fecc8226b71ed67870041c1115204
lanchenghao <lanchenghao@weidian.com>
2016-11-15 20:27:02 +0800

    纠正翻译错误

--- static/js/moudle/data/lang.js
+++ static/js/moudle/data/lang.js
@@ -142,11 +142,13 @@ define([],function(){
             H5_ADVICE_FILL:'邮编：选填，建议填写',
             H5_INS_INPUT:"输入Instagram账户",
             H5_INS_TIP:"注：请确保你的Instagram账户没有设置为“私密账户”",
-            H5_INS_INPUT_PLACEHOLDER:"如：instashop_ina"
+            H5_INS_INPUT_PLACEHOLDER:"如：instashop_ina",
+            H5_INS_VERIFY:"请输入正确的Instagram账户"
 
         },
 
         id : {
+            'H5_INS_VERIFY':"Silakan masukkan Instagram akun",
             'H5_INS_INPUT_PLACEHOLDER':"Contoh: instashop_ina",
             //注：请确保你的Instagram账户没有设置为“私密账户”
             'H5_INS_TIP':"Pastikan akun Instagrammu tidak diprivate",
