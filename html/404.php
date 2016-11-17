276a39f822d3b2ea244d65ff78b83fcb5cda0813
sunchengbin <sunchengbin@weidian.com>
2016-11-17 16:35:06 +0800

    添加ga跳转到官网首页统计,购物车流程头部样式修改

--- html/404.php
+++ html/404.php
@@ -49,7 +49,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
 </head>
 <body>
     <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
-    <script src="<?=STATIC_HOST?>/js/dist/app/404.js?v=1479261539659"></script>
+    <script src="<?=STATIC_HOST?>/js/dist/app/404.js?v=1479371399557"></script>
     <script>
         var _body = document.querySelector('body');
         var _htm = '<div class=""><img src="<?=STATIC_HOST?>/images/app/404.png"/>';
