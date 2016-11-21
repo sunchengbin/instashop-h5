1eb68f0440c9c5055090b6c002fd4d0466c6ef4e
sunchengbin <sunchengbin@weidian.com>
2016-11-21 15:18:09 +0800

    预热banner页面上线

---
+++ html/act/act1201.php
@@ -0,0 +1,41 @@
+<?php
+include_once( dirname(__FILE__).'/../../html/router/common.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1, minimum-scale=1.0, maximum-scale=1.0,user-scalable=no">
+    <meta http-equiv="pragma" content="no-cache">
+    <meta http-equiv="cache-control" content="no-cache">
+    <meta http-equiv="expect" content="0">
+    <meta name="format-detection" content="telephone=no" />
+    <title>domain web gratis datang lagi</title>
+    <style>
+    *{
+        margin: 0;
+        padding: 0;
+        -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+        -webkit-touch-callout: none;
+        background: transparent;
+        outline: none;
+    }
+    img{
+        width:100%;
+    }
+    </style>
+</head>
+<body>
+        <section class="banner">
+            <img src="images/1201.png">
+        </section>
+        <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-78448705-7', 'auto');
+        ga('send', 'pageview');
+        </script>
+</body>
+</html>
