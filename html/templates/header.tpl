7e9654fab829af00ee555a9b3341713841545f8e
sunchengbin <sunchengbin@weidian.com>
2016-11-22 16:12:18 +0800

    上线前修改版本号

--- html/templates/header.tpl
+++ html/templates/header.tpl
@@ -11,6 +11,6 @@
   {$STATIC_ICO_CSS}
   {$STATIC_FONT_CSS}
   {$INDEX_TITLE}
-  <link href="{$STATIC_HOST}/css/dist/app/{$INDEX_CSS_NAME}.css?v=1479790786077" rel="stylesheet"/>
+  <link href="{$STATIC_HOST}/css/dist/app/{$INDEX_CSS_NAME}.css?v=1479801841165" rel="stylesheet"/>
 </head>
 <body>
--- html/usertest.php
+++ html/usertest.php
@@ -145,6 +145,6 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     </section>
     <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
     <script src="<?=STATIC_HOST?>/js/base/require-config.js"></script>
-    <script src="<?=STATIC_HOST?>/js/app/usertest.js?v=1479790785976"></script>
+    <script src="<?=STATIC_HOST?>/js/app/usertest.js?v=1479801841083"></script>
 </body>
 </html>
