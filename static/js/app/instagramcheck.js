d54b68bcef2e33949c0b90efa6bca342221dec74
lanchenghao <lanchenghao@weidian.com>
2016-11-24 16:40:52 +0800

    instagram授权

--- static/js/app/instagramcheck.js
+++ static/js/app/instagramcheck.js
@@ -14,7 +14,6 @@ require(['config', 'base', 'lang', 'common'], function (Config, Base, Lang, Comm
                 action: "instagram",
                 _debug_env: "3.6"
             }
-            //"m-test.instashop.co.id/html/instagramcheck.php?wduss=1k29nj9vdh6Pz/jqIZtKWdbTLsYA7YzMfdjiJm4UrQI=&seller_id=40687"
             var reqUrl = Config.host.phpHost + Config.actions.instagramcheck + "?param=" + JSON.stringify(_reqData);
             window.location.href = reqUrl;
         }
