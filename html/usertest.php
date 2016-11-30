8bdf18eb5adb9dde9f79199d8c25e6cffcbcf2d3
sunchengbin <sunchengbin@weidian.com>
2016-11-30 18:41:58 +0800

    ios隐藏bbm

--- html/usertest.php
+++ html/usertest.php
@@ -145,6 +145,6 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     </section>
     <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
     <script src="<?=STATIC_HOST?>/js/base/require-config.js"></script>
-    <script src="<?=STATIC_HOST?>/js/app/usertest.js?v=1480490722955"></script>
+    <script src="<?=STATIC_HOST?>/js/app/usertest.js?v=1480502455239"></script>
 </body>
 </html>
