21b7e50f6866e0539744fa956e2653623e34a058
lanchenghao <lanchenghao@weidian.com>
2016-12-06 11:15:25 +0800

    update txt

--- static/js/app/act/domainname.js
+++ static/js/app/act/domainname.js
@@ -361,8 +361,8 @@ require(['config', 'insjs', 'ajax', 'dialog', 'fastclick', 'common', 'lang'], fu
             _htm = '<div class="invite-dialog">' +
                 '    <div class="invite-dialog-input">' +
                 '        <textarea name="content" value="" id="j_invite_txt"' +
-                '                  >Hi! Sekarang bikin web ga perlu bayar jutaan rupiah, karena sudah ada INSTASHOP,aplikasi untuk membuat \& mengelola webstore. Pakai KODE REFERRAL ' + _this.user_info.seller_id +
-                ' saat registrasi untuk membuat webstore GRATIS!Download sekarang http://www.instashop.co.id</textarea>' +
+                '                  >Hi! Sekarang bikin web ga perlu bayar mahal,karena sudah ada INSTASHOP,aplikasi untuk membuat \& mengelola webstore. Pakai KODE REFERRAL ' + _this.user_info.seller_id +
+                ' saat registrasi untuk mendapat webstore GRATIS!Download yuk http://www.instashop.co.id</textarea>' +
                 '    </div>' +
                 '    <div class="invite-share-box" data-spider="invitebox">' +
                 '        <ul class="ins-avg-sm-4">' +
