060f5c59b8afaa252a8425d3b89b710f0584b308
sunchengbin <sunchengbin@weidian.com>
2016-12-06 18:27:30 +0800

    3.7上线,显示折扣和编辑模板

--- html/orderconfirm.php
+++ html/orderconfirm.php
@@ -13,7 +13,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     <?=STATIC_DNS?>
     <?=STATIC_ICO_CSS?>
     <?=STATIC_FONT_CSS?>
-    <link href="<?=STATIC_HOST?>/css/dist/app/orderconfirm.css?v=1481019583072" rel="stylesheet"/>
+    <link href="<?=STATIC_HOST?>/css/dist/app/orderconfirm.css?v=1480588647499" rel="stylesheet"/>
     <title>Order</title>
     <script>
         <?php
@@ -43,7 +43,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
         ];
         $pathPrice = 'v1/shopsDiscount';
         ?>
-        
+
         var api_url = '<?php echo check_api($path, $params); ?>';
         var api_url_price = '<?php echo check_api($pathPrice, $paramsPrice); ?>';
         var express_data = JSON.parse(<?php echo get_init_data($path, $params); ?>);
@@ -55,7 +55,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
 <body>
     <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
     <!--<script src="<?=STATIC_HOST?>/js/base/require-config.js"></script>-->
-    <script src="<?=STATIC_HOST?>/js/dist/app/orderconfirm.js?v=1481019583072"></script>
+    <script src="<?=STATIC_HOST?>/js/dist/app/orderconfirm.js?v=1480578543489"></script>
     <script>
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
