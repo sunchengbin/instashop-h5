ecb71aa526d0e6f5c9c202a96b3c05e48d09f2f8
sunchengbin <sunchengbin@weidian.com>
2016-12-07 14:54:03 +0800

    修改店铺详情店招热区

--- html/templates_c/24d16bc955f8502ea76f84211190205401146590_0.file.header.tpl.php
+++ html/templates_c/24d16bc955f8502ea76f84211190205401146590_0.file.header.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2016-12-06 18:21:37
+/* Smarty version 3.1.30, created on 2016-12-07 14:51:21
   from "/Users/sunchengbin/workspace/instashop/instashop-new/html/templates/header.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_58469131e35986_17088602',
+  'unifunc' => 'content_5847b1691c8ec6_74484727',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '24d16bc955f8502ea76f84211190205401146590' => 
     array (
       0 => '/Users/sunchengbin/workspace/instashop/instashop-new/html/templates/header.tpl',
-      1 => 1481019583,
+      1 => 1481090018,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_58469131e35986_17088602 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5847b1691c8ec6_74484727 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -42,7 +42,7 @@ function content_58469131e35986_17088602 (Smarty_Internal_Template $_smarty_tpl)
 
   <link href="<?php echo $_smarty_tpl->tpl_vars['STATIC_HOST']->value;?>
 /css/dist/app/<?php echo $_smarty_tpl->tpl_vars['INDEX_CSS_NAME']->value;?>
-.css?v=1481019583187" rel="stylesheet"/>
+.css?v=1481090018813" rel="stylesheet"/>
 </head>
 <body data-spider="615lnd28"><?php }
 }
