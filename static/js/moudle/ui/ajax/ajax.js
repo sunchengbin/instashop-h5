21921d42f2e075c49eef942346d98ac79b15f368
sunchengbin <sunchengbin@weidian.com>
2016-12-07 13:48:11 +0800

    php和js中提取公用wduss,seller_id并调用

--- static/js/moudle/ui/ajax/ajax.js
+++ static/js/moudle/ui/ajax/ajax.js
@@ -3,8 +3,7 @@
  */
 define(['config','base'],function(Config,Base){
     var Debug = Base.others.getUrlPrem('_debug_env') || localStorage.getItem('DebugEnv');
-    alert(location.href);
-    alert(Debug);
+    Debug = Debug && Debug == 'null'?false:Debug;
     var Ajax = {
         getJsonp : function ( url, success, error ) {
             var _data = Base.others.getUrlPrem('param',url)?JSON.parse(Base.others.getUrlPrem('param',url)):null;
