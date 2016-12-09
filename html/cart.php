b1aaec674cdce36f6bc755413ab6bdfba973c32b
lanchenghao <lanchenghao@weidian.com>
2016-12-09 10:33:19 +0800

    cart 埋点

--- html/cart.php
+++ html/cart.php
@@ -27,7 +27,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
 
       ga('create', 'UA-78448705-7', 'auto');
       ga('send', 'pageview');
-
+      <?=BI_SCRIPT?>
     </script>
 </body>
 </html>
