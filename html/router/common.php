e45cc3fdaeb1843eb5f51417114e541fc3f10ef9
sunchengbin <sunchengbin@weidian.com>
2016-12-13 11:58:00 +0800

    添加统计后,line不能正常添加好友问题.禁用href自动跳转

--- html/router/common.php
+++ html/router/common.php
@@ -34,7 +34,7 @@ function loadClass($strClassName)
     }
 }
 function getFontCss($url){
-    return '<style>@font-face {font-family: "iconfont";src: url("'.$url.'/css/base/fonts/iconfont.ttf?v=1481538463977") format("truetype"),url("'.$url.'/css/base/fonts/iconfont.svg?v=1481538463977#iconfont") format("svg");}</style>';
+    return '<style>@font-face {font-family: "iconfont";src: url("'.$url.'/css/base/fonts/iconfont.ttf?v=1481601323742") format("truetype"),url("'.$url.'/css/base/fonts/iconfont.svg?v=1481601323742#iconfont") format("svg");}</style>';
 }
 function getIco($url){
     return '<link rel="shortcut icon" href="'.$url.'/favicon.ico" type="image/vnd.microsoft.icon"><link rel="icon" href="'.$url.'/favicon.ico" type="image/vnd.microsoft.icon">';
