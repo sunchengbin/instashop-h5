6a55a4de513735ef1179d6b42e07663548a8e4dc
sunchengbin <sunchengbin@weidian.com>
2016-12-28 16:05:49 +0800

    一行三件商品

--- static/js/base/insjs.js
+++ static/js/base/insjs.js
@@ -193,15 +193,15 @@ define([],function(){
 		testJudgeVersion : function(){
 			var _getCurVersion = "";
 			var _getCurVersionFloat = "";
+			_getCurVersion = WIN.navigator.userAgent.match(/Instashop\-(.+?)\-/)[1]||"";
+			_getCurVersionFloat = parseFloat(_getCurVersion.substr(0,3));//目前只支持取前两位'3.5.1'=>3.5 '3.5'=>3.5
+			targetVersion = parseFloat(targetVersion);
+			//比较 例:_getCurVersionFloat 3.5| targetVersion 3.4
+			//符合
+			alert(_getCurVersionFloat);
+			alert(targetVersion);
 			try {
-				//提取版本号 从Instashop-3.5.1/3.5-ios/Android
-				_getCurVersion = WIN.navigator.userAgent.match(/Instashop\-(.+?)\-/)[1]||"";
-				_getCurVersionFloat = parseFloat(_getCurVersion.substr(0,3));//目前只支持取前两位'3.5.1'=>3.5 '3.5'=>3.5
-				targetVersion = parseFloat(targetVersion);
-				//比较 例:_getCurVersionFloat 3.5| targetVersion 3.4
-				//符合
-				alert(_getCurVersionFloat);
-				alert(targetVersion);
+
 				if(_getCurVersionFloat>=targetVersion){
 					return true;
 				}else{//不符合
