5e327f493cbc15e2a5e2e8912b2ba244e55265b4
lanchenghao <lanchenghao@weidian.com>
2017-01-06 10:40:55 +0800

    item报错隐藏

--- static/js/app/item.js
+++ static/js/app/item.js
@@ -5,7 +5,7 @@
  * Created by sunchengbin on 16/6/8.
  * 商品详情页
  */
-require(['lang', 'lazyload', 'ajax', 'config', 'base', 'common', 'buyplug', 'slide', 'cart', 'fastclick', 'contact', 'viewer', 'item','dialog'], function (Lang, Lazyload,  Ajax, Config, Base, Common, Buyplug, Slide, Cart, Fastclick, Contact, Viewer, Item,Dialog) {
+require(['lang', 'lazyload', 'ajax', 'config', 'base', 'common', 'buyplug', 'slide', 'cart', 'fastclick', 'contact', 'viewer', 'item', 'dialog','debug'], function (Lang, Lazyload, Ajax, Config, Base, Common, Buyplug, Slide, Cart, Fastclick, Contact, Viewer, Item, Dialog,Debug) {
     var ITEM = {
         init: function () {
             var _this = this,
@@ -14,44 +14,51 @@ require(['lang', 'lazyload', 'ajax', 'config', 'base', 'common', 'buyplug', 'sli
                 if (_cart_num > 0) {
                     $('.j_cart_wraper').append('<span class="cart-num">' + _cart_num + '</span>');
                 }
-                Lazyload();
-                var _slide = Slide.createNew({
-                    dom: document.querySelector('.j_banner'),
-                    needTab: true,
-                    auto: false
-                });
-                Buyplug({
-                    data: init_data,
-                    noStockCallback : function(){
-                        if ($('.j_show_contact').length) {
-                            _this.contact = Contact({
-                                data: {
-                                    tel: init_data.item.shop.phone,
-                                    line: init_data.item.shop.line_url
-                                },
-                                lang: Lang
-                            });
-                            _this.contact.createHtm({
-                                data: {
-                                    tel: init_data.item.shop.phone,
-                                    line: init_data.item.shop.line_url
-                                },
-                                lang: Lang
-                            }).toShow();
-                        }else{
-                            location.href = init_data.item.shop.line_url;
+                try {
+                    Lazyload();
+                    var _slide = Slide.createNew({
+                        dom: document.querySelector('.j_banner'),
+                        needTab: true,
+                        auto: false
+                    });
+                    Buyplug({
+                        data: init_data,
+                        noStockCallback: function () {
+                            if ($('.j_show_contact').length) {
+                                _this.contact = Contact({
+                                    data: {
+                                        tel: init_data.item.shop.phone,
+                                        line: init_data.item.shop.line_url
+                                    },
+                                    lang: Lang
+                                });
+                                _this.contact.createHtm({
+                                    data: {
+                                        tel: init_data.item.shop.phone,
+                                        line: init_data.item.shop.line_url
+                                    },
+                                    lang: Lang
+                                }).toShow();
+                            } else {
+                                location.href = init_data.item.shop.line_url;
+                            }
                         }
-                    }
-                });
-                Viewer({
-                    btn: '.j_banner li',
-                    images: init_data.item.imgs
-                }).init();
-                Viewer({
-                    btn: '.slide_arrow',
-                    images: init_data.item.imgs
-                }).init();
-                _this.handleFn();
+                    });
+                    Viewer({
+                        btn: '.j_banner li',
+                        images: init_data.item.imgs
+                    }).init();
+                    Viewer({
+                        btn: '.slide_arrow',
+                        images: init_data.item.imgs
+                    }).init();
+                    _this.handleFn();
+                } catch (error) {
+                    Debug.log({
+                        title:"item.js init fail",
+                        data:error
+                    })
+                }
             }
         },
         handleFn: function () {
@@ -63,10 +70,14 @@ require(['lang', 'lazyload', 'ajax', 'config', 'base', 'common', 'buyplug', 'sli
                 $('body').on('click', '.j_down_box', function () {
                     if ($('.j_down_btn').is('.down-btn')) {
                         $('.j_down_btn').removeClass('down-btn').addClass('up-btn');
-                        $('.txt').css({'maxHeight': 'none'});
+                        $('.txt').css({
+                            'maxHeight': 'none'
+                        });
                     } else {
                         $('.j_down_btn').removeClass('up-btn').addClass('down-btn');
-                        $('.txt').css({'maxHeight': '44px'});
+                        $('.txt').css({
+                            'maxHeight': '44px'
+                        });
                     }
                 });
             }
@@ -80,17 +91,17 @@ require(['lang', 'lazyload', 'ajax', 'config', 'base', 'common', 'buyplug', 'sli
             });
             //item=back是为了让返回首页的时候滚动到指定的scrolltop高度
             $('body').on('click', '.j_go_back', function () {
-                PaqPush && PaqPush('返回','');
+                PaqPush && PaqPush('返回', '');
                 //_paq.push(['trackEvent', '返回', 'click', '']);
                 var _local_url = localStorage.getItem('FromUrl'),
                     _host_url = location.href,
                     _key = Base.others.getUrlPrem('search'),
-                    _search_url = Config.host.hrefUrl + 'search.php'+(_key?'?key='+_key+'&seller_id='+init_data.item.seller_id:'');
+                    _search_url = Config.host.hrefUrl + 'search.php' + (_key ? '?key=' + _key + '&seller_id=' + init_data.item.seller_id : '');
                 if (_local_url && !/detail/g.test(_local_url)) {
-                    if (/\.instashop\.co\.id\/\d+/g.test(_local_url)) {//我们自己的域名下
-                        if(/\?search/g.test(_host_url)){//搜索页
+                    if (/\.instashop\.co\.id\/\d+/g.test(_local_url)) { //我们自己的域名下
+                        if (/\?search/g.test(_host_url)) { //搜索页
                             location.href = _search_url;
-                        }else{
+                        } else {
                             if (/\/s\//g.test(_local_url)) {
                                 location.href = _this.transUrl(_local_url);
                             } else {
@@ -102,16 +113,16 @@ require(['lang', 'lazyload', 'ajax', 'config', 'base', 'common', 'buyplug', 'sli
                                 }
                             }
                         }
-                    } else {//独立域名
+                    } else { //独立域名
                         var _host_name = location.hostname;
-                        if(/\?search/g.test(_host_url)){//搜索页
+                        if (/\?search/g.test(_host_url)) { //搜索页
                             location.href = _search_url;
-                        }else {
-                            if (/\/\d+/g.test(_local_url)) {//是当前详情页
-                                if (/\/k\/\d+/g.test(_local_url)) {//分类页
+                        } else {
+                            if (/\/\d+/g.test(_local_url)) { //是当前详情页
+                                if (/\/k\/\d+/g.test(_local_url)) { //分类页
                                     location.href = _this.transUrl(_local_url);
                                 } else {
-                                    if (/\/s\//g.test(_local_url)) {//m.instashop域名规则首页
+                                    if (/\/s\//g.test(_local_url)) { //m.instashop域名规则首页
                                         location.href = _this.transUrl(_local_url);
                                     } else {
                                         location.href = location.protocol + '//' + _host_name + '?item=back';
@@ -136,27 +147,27 @@ require(['lang', 'lazyload', 'ajax', 'config', 'base', 'common', 'buyplug', 'sli
             $('body').on('click', '.j_cart_wraper', function () {
                 var _this = $(this),
                     _url = _this.attr('data-url');
-                    PaqPush && PaqPush('查看购物车','');
+                PaqPush && PaqPush('查看购物车', '');
                 Common.saveFromUrl(function () {
                     location.href = _url;
                 });
             });
             //满减 lanchenghao
-            $('body').on('click','.j_reduc_box',function(){
+            $('body').on('click', '.j_reduc_box', function () {
                 var _htm = '<ul class="reduc-rule-list">';
-                if(!!init_data.item.shop.shop_discount){
-                    PaqPush && PaqPush('查看满减活动公告','');
+                if (!!init_data.item.shop.shop_discount) {
+                    PaqPush && PaqPush('查看满减活动公告', '');
                     //_paq.push(['trackEvent', '查看满减活动公告', 'click', '']);
-                    for(var i=0,_reducItem;_reducItem=init_data.item.shop.shop_discount.info[i++];){
-                        _htm+="<li><span></span>Minimal Pembelian Rp "+Base.others.priceFormat(_reducItem.condition_price)+" Potongan Rp "+ Base.others.priceFormat(_reducItem.discount_price)+"</li>"
+                    for (var i = 0, _reducItem; _reducItem = init_data.item.shop.shop_discount.info[i++];) {
+                        _htm += "<li><span></span>Minimal Pembelian Rp " + Base.others.priceFormat(_reducItem.condition_price) + " Potongan Rp " + Base.others.priceFormat(_reducItem.discount_price) + "</li>"
                     }
-                    _htm +='<li><span></span>'+$(".reduc-expire").text()+'</li></ul>';
+                    _htm += '<li><span></span>' + $(".reduc-expire").text() + '</li></ul>';
                     // _htm = _htm.replace(/,$/gi,'') +"</br>"+ $(".reduc-expire").text();
                     Dialog.alert({
-                        top_txt:"<p style='text-align:center'>"+Lang.H5_REDUC_TITLE+"</p>",
-                        show_top:true,
-                        body_txt:_htm,
-                        body_fn:function(){
+                        top_txt: "<p style='text-align:center'>" + Lang.H5_REDUC_TITLE + "</p>",
+                        show_top: true,
+                        body_txt: _htm,
+                        body_fn: function () {
                             $('.j_c_btn').hide();
                         }
                     })
@@ -181,9 +192,9 @@ require(['lang', 'lazyload', 'ajax', 'config', 'base', 'common', 'buyplug', 'sli
                     }).toShow();
                 });
             }
-            $('body').on('click','.j_goto_line',function(){
-                    location.href = init_data.item.shop.line_url;
-                })
+            $('body').on('click', '.j_goto_line', function () {
+                location.href = init_data.item.shop.line_url;
+            })
         },
         transUrl: function (url) {
             if (/\?/g.test(url)) {
