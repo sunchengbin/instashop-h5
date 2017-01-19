eb4bf720ce06514307a31ee7b3e5718e2ae32e0f
sunchengbin <sunchengbin@weidian.com>
2017-01-19 13:47:03 +0800

    修改购物车中有sku的商品bug

--- html/router/common.php
+++ html/router/common.php
@@ -34,7 +34,7 @@ function loadClass($strClassName)
     }
 }
 function getFontCss($url){
-    return '<style>@font-face {font-family: "iconfont";src: url("'.$url.'/css/base/fonts/iconfont.ttf?v=1484733517490") format("truetype"),url("'.$url.'/css/base/fonts/iconfont.svg?v=1484733517490#iconfont") format("svg");}</style>';
+    return '<style>@font-face {font-family: "iconfont";src: url("'.$url.'/css/base/fonts/iconfont.ttf?v=1484804749674") format("truetype"),url("'.$url.'/css/base/fonts/iconfont.svg?v=1484804749674#iconfont") format("svg");}</style>';
 }
 function getIco($url){
     return '<link rel="shortcut icon" href="'.$url.'/favicon.ico" type="image/vnd.microsoft.icon"><link rel="icon" href="'.$url.'/favicon.ico" type="image/vnd.microsoft.icon">';
--- html/usertest.php
+++ html/usertest.php
@@ -145,6 +145,6 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     </section>
     <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
     <script src="<?=STATIC_HOST?>/js/base/require-config.js"></script>
-    <script src="<?=STATIC_HOST?>/js/app/usertest.js?v=1484733517138"></script>
+    <script src="<?=STATIC_HOST?>/js/app/usertest.js?v=1484804749440"></script>
 </body>
 </html>
