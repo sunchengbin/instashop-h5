df10848b7c7a68beac5664fffcc4f4e12817c183
sunchengbin <sunchengbin@weidian.com>
2017-01-19 11:02:20 +0800

    online-test

--- online.sh
+++ online.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 echo '切换到master'
-#git checkout master
+git checkout 4.0
 git pull
 echo '开始构建'
 #./build.sh
 echo '开始commit代码'
 git commit -a -m $1
 echo '开始push代码'
-#git push origin master
+git push origin 4.0
 echo '开始创建tag'
 git tag $2
 echo '开始push tag'
