ff3d63b814f0ed0e5e1e8075295ed3c5d64a4794
sunchengbin <sunchengbin@weidian.com>
2017-01-19 10:15:09 +0800

    test

--- online.sh
+++ online.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
 echo '切换到master'
-git checkout master
+#git checkout master
 echo '开始构建'
 ./build.sh
 echo '开始commit代码'
+git commit
 echo '开始push代码'
 echo '开始创建tag'
 echo '开始push tag'
