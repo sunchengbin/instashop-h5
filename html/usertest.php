77459f17013d2355fca95c614a3324c9c81f5ee4
sunchengbin <sunchengbin@weidian.com>
2017-02-16 16:37:58 +0800

    orderdetail价格显示顺序修改

--- html/usertest.php
+++ html/usertest.php
@@ -145,6 +145,6 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
         <p>loading...</p>
     </section>
     <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
-    <script src="<?=STATIC_HOST?>/js/dist/app/usertest.js?v=1486622410152"></script>
+    <script src="<?=STATIC_HOST?>/js/dist/app/usertest.js?v=1487234271604"></script>
 </body>
 </html>
