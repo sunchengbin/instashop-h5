cc9bc958ebde4659941ec941d91d373ee6f74614
sunchengbin <sunchengbin@weidian.com>
2017-02-21 09:32:01 +0800

    获取优惠券添加判断统计

--- html/usertest.php
+++ html/usertest.php
@@ -145,6 +145,6 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
         <p>loading...</p>
     </section>
     <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
-    <script src="<?=STATIC_HOST?>/js/dist/app/usertest.js?v=1487333532207"></script>
+    <script src="<?=STATIC_HOST?>/js/dist/app/usertest.js?v=1487640702382"></script>
 </body>
 </html>
