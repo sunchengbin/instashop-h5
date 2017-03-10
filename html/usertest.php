347d7dd4750ca017a5c6199c8f16de03bb6e5efd
lanchenghao <lanchenghao@weidian.com>
2017-03-10 14:52:20 +0800

    编译更新

--- html/usertest.php
+++ html/usertest.php
@@ -145,6 +145,6 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
         <p>loading...</p>
     </section>
     <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
-    <script src="<?=STATIC_HOST?>/js/dist/app/usertest.js?v=1489056563598"></script>
+    <script src="<?=STATIC_HOST?>/js/dist/app/usertest.js?v=1489128726001"></script>
 </body>
 </html>
