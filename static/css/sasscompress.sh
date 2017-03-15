9ee1dc336b249c9126ffd637373e970dd8719807
sunchengbin <sunchengbin@weidian.com>
2017-03-15 19:03:47 +0800

    新模板sass构建脚本

--- static/css/sasscompress.sh
+++ static/css/sasscompress.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 if [ $1 ]
 then
-    for file in ./$1/app/*.scss
-    do
-    if test -f $file
+    if test -d ./$1/app
     then
-        resFile=`basename $file`
-        sass --style compressed $file dist/$1/app/${resFile%.*}.css
-        echo dist/$1/${resFile%.*}.css
+        for file in ./$1/app/*.scss
+        do
+        if test -f $file
+        then
+            resFile=`basename $file`
+            sass --style compressed $file dist/$1/app/${resFile%.*}.css
+            echo dist/$1/${resFile%.*}.css
+        else
+            echo $file 'is dir'
+        fi
+        done
     else
-        echo $file 'dir'
+        echo ./$1' folder not exist'
     fi
-    done
 else
     echo '请指明文件夹路径'
+    echo 'example: ./sasscompress.sh first'
 fi
