3c920e7dc0c852bccdcaaa8fd7457d277964679c
lanchenghao <lanchenghao@weidian.com>
2017-03-24 15:02:24 +0800

    更新版本号

--- html/address.php
+++ html/address.php
@@ -13,13 +13,13 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     <?=STATIC_DNS?>
     <?=STATIC_ICO_CSS?>
     <?=STATIC_FONT_CSS?>
-    <link href="<?=STATIC_HOST?>/css/dist/app/address.css?v=1490264421698" rel="stylesheet"/>
+    <link href="<?=STATIC_HOST?>/css/dist/app/address.css?v=1490338931738" rel="stylesheet"/>
     <title>Address</title>
 </head>
 <body data-spider="oxv83yea">
     <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
     <!--<script src="<?=STATIC_HOST?>/js/base/require-config.js"></script>-->
-    <script src="<?=STATIC_HOST?>/js/dist/app/address.js?v=1490264421698"></script>
+    <script src="<?=STATIC_HOST?>/js/dist/app/address.js?v=1490338931738"></script>
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
--- static/js/moudle/data/lang.js
+++ static/js/moudle/data/lang.js
@@ -282,7 +282,7 @@ define(['base'], function (Base) {
             'H5_SHIPPING_COST': 'Biaya Pengiriman',
 
             // 价格
-            'H5_PRICE': 'Harga',
+            'H5_PRICE': 'Harga Jualmu',
 
             // 型号
             'H5_ITEM_SKU': 'Model',
