194168c563c8ffdae37c9ed179ddc7bfe3e52d7e
lanchenghao <lanchenghao@weidian.com>
2017-04-01 16:29:08 +0800

    更新facebook分享

--- static/js/moudle/app/sharebargain/sharebargain.js
+++ static/js/moudle/app/sharebargain/sharebargain.js
@@ -33,7 +33,7 @@ define(['dialog','lang','base'],function(Dialog,Lang,Base){
                 +'</div>'
                 +'<div class="share-operate bargain_inv_operate clearfix">'
                 // +'<a href="javascript:;" class="j_share_action" data-type="line" spm-auto="优惠券分享到facebook" spm-click="" data-url="http://www.facebook.com/share.php?t=">'
-                +'<a href="javascript:;" class="j_share_action" data-type="line" spm-auto="优惠券分享到facebook" spm-click="" data-url="facebook://send?t=">'
+                +'<a href="javascript:;" class="j_share_action" data-type="line" spm-auto="优惠券分享到facebook" spm-click="" data-url="fb://facewebmodal/f?href=http://www.facebook.com/share.php?t=">'
                 +'<i class="iconfont icon-share-facebook" ></i>'
                 +'<p>FB</p>'
                 +'</a>'
