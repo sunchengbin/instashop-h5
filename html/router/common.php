8d3acb8cdc7bdb16ae423416cc9cd3b19acd2c61
lanchenghao <lanchenghao@weidian.com>
2017-04-05 16:12:31 +0800

    111

--- html/router/common.php
+++ html/router/common.php
@@ -34,7 +34,7 @@ function loadClass($strClassName)
     }
 }
 function getFontCss($url){
-    return '<style>@font-face {font-family: "iconfont";src: url("'.$url.'/css/base/fonts/iconfont.ttf?v=1491379594488") format("truetype"),url("'.$url.'/css/base/fonts/iconfont.svg?v=1491379594488#iconfont") format("svg");}</style>';
+    return '<style>@font-face {font-family: "iconfont";src: url("'.$url.'/css/base/fonts/iconfont.ttf?v=1491379929424") format("truetype"),url("'.$url.'/css/base/fonts/iconfont.svg?v=1491379929424#iconfont") format("svg");}</style>';
 }
 function getIco($url){
     return '<link rel="shortcut icon" href="'.$url.'/favicon.ico" type="image/vnd.microsoft.icon"><link rel="icon" href="'.$url.'/favicon.ico" type="image/vnd.microsoft.icon">';
