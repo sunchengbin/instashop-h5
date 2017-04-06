82823553d59ee4e26ce5dea2b38b8cfe53be082b
lanchenghao <lanchenghao@weidian.com>
2017-04-06 14:23:27 +0800

    优化

--- html/im.php
+++ html/im.php
@@ -13,7 +13,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     <?=STATIC_ICO_CSS?>
     <?=STATIC_FONT_CSS?>
     <title></title>
-    <link href="<?=STATIC_HOST?>/css/dist/app/im_index.css?v=1491453753990" rel="stylesheet">
+    <link href="<?=STATIC_HOST?>/css/dist/app/im_index.css?v=1491459800044" rel="stylesheet">
 </head>
 <body>
     <nav class="header ks-clear">
