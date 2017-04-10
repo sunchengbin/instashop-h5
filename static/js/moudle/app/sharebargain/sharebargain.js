f1d5a7ea434182e79d916cb1eea47f32c372a7e1
lanchenghao <lanchenghao@weidian.com>
2017-04-10 12:36:35 +0800

    还原回去

--- static/js/moudle/app/sharebargain/sharebargain.js
+++ static/js/moudle/app/sharebargain/sharebargain.js
@@ -36,7 +36,7 @@ define(['dialog','lang','base'],function(Dialog,Lang,Base){
                 +'<i class="iconfont icon-share-facebook" ></i>'
                 +'<p>Facebook</p>'
                 +'</a>'
-                +'<a href="javascript:;" class="j_share_action" data-type="line" spm-auto="砍价活动分享到line" spm-click="" data-url="line://msg/text/">'
+                +'<a href="javascript:;" class="j_share_action" data-type="line" spm-auto="砍价活动分享到line" spm-click="" data-url="http://line.naver.jp/R/msg/text/?">'
                 +'<i class="iconfont icon-share-line" ></i>'
                 +'<p>LINE</p>'
                 +'</a>'
@@ -68,8 +68,8 @@ define(['dialog','lang','base'],function(Dialog,Lang,Base){
                         location.href = _fb_url;
                     }else{
                         // location.href = _dom.attr('data-url') + encodeURIComponent(_this.config.bargain_inv_url);
-                        location.href = "line://msg/text/https%3A%2F%2Ftw.news.yahoo.com%2F228-p-072909137.html%3Fsoc_src%3Dunv-sh%26soc_trk%3Dli";
-                        // location.href = _dom.attr('data-url') + encodeURIComponent(_this.share_content);
+                        // location.href = "line://msg/text/https%3A%2F%2Ftw.news.yahoo.com%2F228-p-072909137.html%3Fsoc_src%3Dunv-sh%26soc_trk%3Dli";
+                        location.href = _dom.attr('data-url') + encodeURIComponent(_this.share_content);
                     }
 
                 },100);
