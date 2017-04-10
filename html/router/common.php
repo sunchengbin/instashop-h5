de5f9d4a6ad47f2289b43c728a6be6fa41a9e269
lanchenghao <lanchenghao@weidian.com>
2017-04-10 14:46:13 +0800

    更新文案

--- html/router/common.php
+++ html/router/common.php
@@ -34,7 +34,7 @@ function loadClass($strClassName)
     }
 }
 function getFontCss($url){
-    return '<style>@font-face {font-family: "iconfont";src: url("'.$url.'/css/base/fonts/iconfont.ttf?v=1491805657545") format("truetype"),url("'.$url.'/css/base/fonts/iconfont.svg?v=1491805657545#iconfont") format("svg");}</style>';
+    return '<style>@font-face {font-family: "iconfont";src: url("'.$url.'/css/base/fonts/iconfont.ttf?v=1491806763560") format("truetype"),url("'.$url.'/css/base/fonts/iconfont.svg?v=1491806763560#iconfont") format("svg");}</style>';
 }
 function getIco($url){
     return '<link rel="shortcut icon" href="'.$url.'/favicon.ico" type="image/vnd.microsoft.icon"><link rel="icon" href="'.$url.'/favicon.ico" type="image/vnd.microsoft.icon">';
