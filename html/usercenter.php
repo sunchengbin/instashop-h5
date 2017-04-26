f49f6f16e61706497bb86aade2803c0f7a00dffa
sunchengbin <sunchengbin@weidian.com>
2017-04-26 18:59:15 +0800

    用户中心和退款申请

--- html/usercenter.php
+++ html/usercenter.php
@@ -6,13 +6,18 @@
     $smarty = smartyCommon('default');
 
     $buyer_id = $_REQUEST['buyer_id'];
+    $seller_id = $_REQUEST['seller_id'];
     $uss = $_REQUEST['uss'];
     $path = 'v1/orders';
     $params = [];
     $params["buyer_id"] = $buyer_id;
     $params["uss"] = $uss;
-    $params["state"] = 'unpaid';
-    $params["warrant"] = 1;
+    $params["page_num"] = 0;
+    $params["page_size"] = 30;
+    $params["seller_id"] = $seller_id;
+    $params["action"] = 'buyer_list';
+
+
     $ret = get_init_php_data($path, $params);
     $json = json_decode($ret, true);
 
