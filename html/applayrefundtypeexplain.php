2637ae04330025bfff77f53f62fa6e0040227495
lanchenghao <lanchenghao@weidian.com>
2017-05-08 17:39:12 +0800

    无需物流

--- html/applayrefundtypeexplain.php
+++ html/applayrefundtypeexplain.php
@@ -58,63 +58,22 @@
     </style>
 </head>
 <body>
-    <h1>Penjelasan Promo "Tawar Harga"</h1>
-    <ul>
-        <li>
-            <a href="#first-type" class="">Sekilas Konsep Promo "Tawar Harga" </a>
-        </li>
-        <li>
-            <a href="#second-type" class="">Langkah-Langkah Untuk Pembeli Mengikuti Promo</a>
-        </li>
-        <li>
-            <a href="#third-type" class="">Contoh Ilustrasi Kasus</a>
-        </li>
-    </ul>
+    <h1>Apa bedanya Rekening Bersama dengan Transfer ke Penjual? (versi pembeli)</h1>
     <section class="content">
-        <h1 id="first-type">Sekilas Konsep Promo "Tawar Harga"</h1>
         <div>
-            <p>
-                Melalui promo ini, <strong>kamu dapat menetapkan harga terendah dari suatu produk yang dapat ditawar oleh pembelimu.</strong>
-            </p>
-            <p>Untuk mendapatkan harga terendah tersebut, pembelimu harus menyebarkan link produk ke teman-temannya. Teman-teman pembelimu dapat membantu menawar harga, hingga harga mencapai batasan terendah yang kamu tetapkan. </p>
-            <p>Harapannya, jika teman-teman pembelimu melihat link ini, mereka juga akan tertarik membeli produk ini, atau bahkan melihat-lihat produk lainnya dari webmu! Lumayan kan, bisa menjaring banyak calon pembeli baru!</p>
-            <p></p>
+            <p><strong>Dengan Rekening Bersama</strong>, dana pembeli akan disimpan sementara di rekening Instashop dan baru dicairkan ke “Saldo Dapat Ditarik” penjual setelah pembeli mengkonfirmasi penerimaan barang.</p>
         </div>
-        <h1 id="second-type">Langkah-Langkah Untuk Pembeli Mengikuti Promo</h1>
         <div>
-            <p>1. Klik link promo yang kamu bagikan</p>
-            <p>2. Klik <strong>"TAWAR PRODUK INI"</strong></p>
-            <p>3. Sistem kami akan <strong>menawar harga secara acak</strong>, sehingga <strong>berkurang</strong> dari harga normal. Jika harga sudah berhasil ditawar, pembeli akan melihat notifikasi “Selamat, Kamu Telah Menawar Rp XXX”</p>
-            <p>4. Setelah itu, jika pembelimu ingin menawar harga lagi hingga mencapai harga terendah, pembeli dapat klik tombol <strong>"TAWAR LAGI”</strong></p>
-            <p>5. Setelah klik tombol <strong>“TAWAR LAGI”</strong>, pembelimu akan diminta untuk login dulu dengan akun LINE / FACEBOOK sebelum melanjutkan</p>
-            <p>6. Setelah login berhasil, pembelimu akan diminta untuk mengajak teman-temannya membantu menawar harga.</p>
-            <p>7. Setelah selesai membagikan ajakan menawar harga ke sosial medianya, pembelimu akan diminta untuk menuliskan nomor ponsel. Kami akan menginformasikan melalui SMS setiap kali ada teman yang membantunya menawar.</p>
-            <p>8. Tampilan halaman ajakan menawar harga yang dilihat teman pembelimu.</p>
-            <p>***Teman pembelimu dapat klik tombol <strong>"Bantu XXX Tawar"</strong> untuk membantu menawar harga. Jika harga sudah berhasil ditawar, teman pembelimu akan melihat notifikasi <strong>“Selamat, Kamu Telah Menawar Rp XXX”. Nominal Tawar ditentukan acak oleh sistem, BUKAN diisi sendiri oleh penawar.</strong></p>
-            <p>***Harga setelah ditawar ini <strong>HANYA</strong> berlaku bagi <strong>pembelimu</strong> saja, <strong>BUKAN</strong> untuk teman yang membantunya menawar. </p>
-            <p>***Jika teman pembelimu ingin ikut serta membeli dengan harga terendah ini, mereka dapat meng-klik tombol <strong>"MAU BELI JUGA"</strong>. Halaman akan dialihkan ke halaman awal produk ini, sehingga teman pembelimu juga dapat meminta teman-teman lainnya untuk membantu menawar harga</p>
-            <p><strong>***Satu akun Facebook / LINE hanya dapat membantu menawar sebanyak satu kali.</strong></p>
-            <p>9.Tampilan halaman yang dilihat oleh pembelimu ketika harga sudah mencapai harga terendah.  Kini pembelimu dapat berbelanja dengan harga termurah ini SELAGI masih berada dalam batasan waktu yang kamu tentukan.</p>
-            <p><strong>Catatan:</strong> Pembelimu dapat berbelanja kapanpun, termasuk ketika harga belum mencapai harga terendah sekalipun, asalkan batasan waktu promo belum berakhir. Jika waktu promo telah berakhir sementara produk belum dibelanjakan, produk akan dibeli dengan harga normal. </p>
+            <p>Pembeli dapat memperpanjang waktu penerimaan produk sebanyak 1 (satu) kali setelah 7 (tujuh) hari sejak resi dimasukkan penjual. Waktu perpanjangan penerimaan produk adalah sebanyak 3 (tiga) hari. Jika setelah 3 (tiga) hari pembeli tidak mengkonfirmasi penerimaan barang atau mengajukan pengembalian dana, maka dana otomatis dicairkan ke “Saldo Dapat Ditarik” penjual dan transaksi dianggap selesai. </p>
         </div>
-        <h1 id="third-type">Contoh Ilustrasi Kasus</h1>
         <div>
-            <p>
-                Contoh ilustrasi:
-            </p>
-            <p>
-                <strong>Harga Awal Produk</strong> = 10.000
-            </p>
-            <p>
-                <strong>Harga Tawar Terendah</strong> = 8.000
-            </p>
-            <p>
-                Pembelimu telah melakukan penawaran pertama sebesar 500 <strong>(ingat, nominal tawar ditentukan acak oleh sistem)</strong>, sehingga <strong>harga menjadi 9.500</strong>.
-            </p>
-            <p>Maka masih ada <strong>selisih sebesar 1.500</strong>, yang perlu dibantu tawar oleh teman-teman pembelimu, sehingga dapat mencapai harga terendah 8.000. Masing-masing akun LINE / Facebook, baik itu pembelimu maupun temannya, HANYA dapat menawar sebanyak <strong>SATU KALI untuk SATU PRODUK.</strong></p>
-            <p>Jumlah teman yang diperlukan untuk bantu menawar hingga dapat mencapai harga terendah berbeda-beda untuk setiap produk, bergantung pada <strong>selisih dari harga awal produk dengan harga tawar terendah</strong>. Semakin jauh selisih harganya, semakin banyak pula jumlah orang yang diperlukan hingga dapat mencapai harga terendah.</p>
-            <p>Misalnya ada 5 orang teman pembelimu yang membantu menawar, masing-masing A, B, C, D, E. Nominal tawar masing-masing orang berbeda dan ditentukan secara acak dari sistem.</p>
-            <p>Misalnya untuk A = nominal tawar 200, untuk B = nominal tawar 300, untuk C = 150, untuk D = 750, untuk E = 100. Setelah E menawar, harga telah mencapai harga terendah yakni <strong>8.000</strong>, sehingga pembelimu dapat membeli / mengajukan pesanan seharga tersebut, selama batasan waktu promo belum berakhir. </p>
+            <p>Setelah barang terkonfirmasi diterima, maka Instashop tidak lagi bertanggung jawab terhadap keamanan transaksi. Segala proses retur atau pengembalian produk setelah itu dilakukan langsung antara pembeli dan penjual.</p>
+        </div>
+        <div>
+            <p>Fitur Rekening Bersama saat ini hanya dapat dinikmati <strong>jika penjual mendukung layanan ini.</strong></p>
+        </div>
+        <div>
+            <p><strong>Dengan Transfer Langsung</strong>, saldo akan segera Instashop cairkan ke rekening penjual setelah pembayaran terverifikasi. Proses pembatalan pesanan dan pengembalian dana dilakukan langsung antara penjual dan pembeli.</p>
         </div>
     </section>
 </body>
