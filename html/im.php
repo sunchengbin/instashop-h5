1e881a9896ceabe3ee6c4e28dc36485c06d50803
lanchenghao <lanchenghao@weidian.com>
2017-05-08 10:08:55 +0800

    修复登录窗口title 定义引发的undefinedwent

--- html/im.php
+++ html/im.php
@@ -13,7 +13,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     <?=STATIC_ICO_CSS?>
     <?=STATIC_FONT_CSS?>
     <title></title>
-    <link href="<?=STATIC_HOST?>/css/dist/app/im_index.css?v=1493980340799" rel="stylesheet">
+    <link href="<?=STATIC_HOST?>/css/dist/app/im_index.css?v=1494209284937" rel="stylesheet">
 </head>
 <body>
     <nav class="header ks-clear">
