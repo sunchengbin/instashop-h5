af49808be5d4816e758a77080b131b49ef6b3a1d
sunchengbin <sunchengbin@weidian.com>
2017-06-15 16:36:08 +0800

    调试添加购物车bug

--- html/usertest.php
+++ html/usertest.php
@@ -145,6 +145,6 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
         <p>loading...</p>
     </section>
     <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
-    <script src="<?=STATIC_HOST?>/js/dist/app/usertest.js?v=1497404852025"></script>
+    <script src="<?=STATIC_HOST?>/js/dist/app/usertest.js?v=1497515758510"></script>
 </body>
 </html>
