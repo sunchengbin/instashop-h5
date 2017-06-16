d0a96029224947af6ec67621e2cac00674d0039b
sunchengbin <sunchengbin@weidian.com>
2017-06-16 11:17:27 +0800

    购物车样式修改,地址管理页面js修改

--- static/js/app/addressadministration.js
+++ static/js/app/addressadministration.js
@@ -4,21 +4,22 @@
 /**
  * Created by sunchengbin on 16/6/12.
  */
-require(['config','base'], function (Config,Base) {
+require(['config','base','fastclick'], function (Config,Base,Fastclick) {
+    Fastclick.attach(document.body);
     $('body').on('click','.j_go_back',function(){
         history.go(-1);
-    });
+    })
     $('body').on('click','.j_address_info',function(){
         var _this = $(this),
             _id = _this.attr('data-id');
         location.href = Config.host.hostUrl+'orderconfirm.php?select_items='+Base.others.getUrlPrem('select_items')+'&address_id='+_id;
-    });
+    })
     $('body').on('click','.j_edit_address',function(){
         var _this = $(this),
             _id = _this.attr('data-id');
         location.href = Config.host.hostUrl+'address.php?select_items='+Base.others.getUrlPrem('select_items')+'&address_id='+_id;
-    });
+    })
     $('body').on('click','.j_add_address',function(){
         location.href = Config.host.hostUrl+'address.php?select_items='+Base.others.getUrlPrem('select_items');
-    });
+    })
 })
