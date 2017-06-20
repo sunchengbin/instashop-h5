0fc7867a40ecf94c6c46ffdde5840255cad56a55
sunchengbin <sunchengbin@weidian.com>
2017-06-20 17:24:53 +0800

    4.9分销商订单详情页和其他页面php文件建立

---
+++ html/distributordetail.php
@@ -0,0 +1,79 @@
+<?php
+    include_once( dirname(__FILE__).'/../html/router/common.php');
+    include_once( dirname(__FILE__).'/../html/router/util.php' );
+    include_once( dirname(__FILE__).'/../html/router/base.php');
+
+    $smarty = smartyCommon('default_scss');
+
+    /*HOST_NAME*/
+    $hostname=$_SERVER['SERVER_NAME'];
+    $smarty->assign('HOST_NAME',HOST_NAME);
+
+    /*获取item页面的数据*/
+    $params = [];
+    $item_id = $_REQUEST['item_id'];
+    if (!$item_id) {
+        $ss = split('\/', $_SERVER['REQUEST_URI']);
+        $item_id = end($ss);
+    }
+    $params["opt"] = 'H5_Detail,bargain';
+    $path = 'v1/items/'.$item_id;
+    $ret = get_init_php_data($path, $params);
+    $json = json_decode($ret, true);
+    $url = $json['item']['img'];
+    $url = str_replace("w=110", "w=140", $url);
+    $url = str_replace("h=110", "h=140", $url);
+
+
+
+
+    $smarty->assign('INDEX_DATA',$json);
+    $smarty->assign('INDEX_DATA_SHOP',$json['item']);
+    $smarty->assign('INDEX_DATA_STR',$ret);
+    $smarty->assign('INDEX_USER_INFO',json_encode($_POST));
+
+    /*item页面支持app分享的数据*/
+    $item_title = '<meta property="og:image" content="'.$url.'"><title>'.$json["item"]["item_name"].'</title>';
+    $smarty->assign('INDEX_TITLE',$item_title);
+
+    /*基础的js,css文件名*/
+    $smarty->assign('INDEX_JS_NAME','distributordetail');
+    $smarty->assign('INDEX_CSS_NAME','distributordetail');
+
+    //请求第三个tab数据
+    // 获取店铺信息
+    $seller_id = $json['item']['shop']['id'];
+    $path = 'v1/shops/'.$seller_id;
+    $paramsForShopInfo = [
+        'action' => 'index_shopinfo',
+        'platform' => 'web'
+    ];
+    $ret = get_init_php_data($path, $paramsForShopInfo);
+    $json = json_decode($ret, true);
+    $smarty->assign('SHOP_INFO_DATA_STR',$ret);
+    $smarty->assign('SHOP_INFO_DATA',$json["shop"]);
+    //获取购物车商品数量
+    $get_cart_num_path = 'v1/buyerCart';
+    $cart_params = [];
+    $uss = $_COOKIE['uss'];
+    $cart_params['action'] = 'num';
+    $cart_params['seller_id'] = $_REQUEST['seller_id'];
+    $cart_params['is_direct_buy'] = 0;
+    if($uss){
+        $cart_params['uss'] = $uss;
+        $cart_params['buyer_id'] = $_COOKIE['uss_buyer_id'];
+    }else{
+        $cart_params['buyer_id'] = $_COOKIE['buyer_id'];
+    }
+    $cart_ret = get_init_php_data($get_cart_num_path, $cart_params);
+    $cart_json = json_decode($cart_ret, true);
+
+    $cart_num = $cart_json["cart_num"];
+    $cart_num = $cart_num?($cart_num>9?'9+':$cart_num):0;
+    $smarty->assign('CART_NUM',$cart_num);
+
+
+    $smarty->display('distributordetail.tpl');
+
+
+?>
---
+++ static/js/app/distributordetail.js
@@ -0,0 +1,104 @@
+/**
+ * Created by sunchengbin on 2017/6/20.
+ * app内嵌分销商商品详情页js
+ */
+require(['lang', 'lazyload', 'ajax', 'config', 'base', 'common', 'buyplug', 'slide', 'cart', 'fastclick', 'contact', 'viewer', 'item', 'dialog', 'debug'], function (Lang, Lazyload, Ajax, Config, Base, Common, Buyplug, Slide, Cart, Fastclick, Contact, Viewer, Item, Dialog, Debug) {
+    var ITEM = {
+        init: function () {
+            var _this = this;
+            if (init_data && init_data.code == 200) {
+
+                if ($('.j_show_contact').length) {
+                    var _contact_data = {
+                        tel: init_data.item.shop.phone,
+                        line: init_data.item.shop.line_url,
+                        whatsapp:init_data.item.shop.whatsapp_url
+                    };
+                    _this.contact = Contact({
+                        data: _contact_data,
+                        btn:'.j_show_contact',
+                        lang: Lang
+                    });
+
+                }
+                try {
+                    Lazyload();
+
+                    //商品图片轮播初始化
+                    var _slide = Slide.createNew({
+                        dom: document.querySelector('.j_banner'),
+                        needTab: true,
+                        auto: false
+                    });
+                    //添加到购物车插件
+                    Buyplug({
+                        data:init_data,
+                        noStockCallback: function () {
+                            if ($('.j_show_contact').length) {
+                                _this.contact && _this.contact.createHtm({
+                                    data: _contact_data,
+                                    lang: Lang
+                                }).toShow();
+                            } else {
+                                location.href = init_data.item.shop.line_url;
+                            }
+                        }
+                    });
+                    //点击轮播图查看原图初始化
+                    Viewer({
+                        btn: '.j_banner li',
+                        images: init_data.item.imgs
+                    }).init();
+                    Viewer({
+                        btn: '.slide_arrow',
+                        images: init_data.item.imgs
+                    }).init();
+
+                    _this.handleFn();
+                } catch (error) {
+                    Debug.log({
+                        title: "item.js init fail",
+                        data: error
+                    })
+                }
+            }
+
+        },
+        handleFn: function () {
+            //初始化fastclick事件
+            Fastclick.attach(document.body);
+            var _this = this;
+            //查看店铺描述
+            if ($('.txt-hide').height() > 44) {
+                $('.down-btn').show();
+                $('body').on('click', '.j_down_box', function () {
+                    if ($('.j_down_btn').is('.down-btn')) {
+                        $('.j_down_btn').removeClass('down-btn').addClass('up-btn');
+                        $('.txt').css({
+                            'maxHeight': 'none'
+                        });
+                    } else {
+                        $('.j_down_btn').removeClass('up-btn').addClass('down-btn');
+                        $('.txt').css({
+                            'maxHeight': '44px'
+                        });
+                    }
+                });
+            }
+            //查看购物车
+            $('body').on('click', '.j_cart_wraper', function () {
+                var _this = $(this),
+                    _url = _this.attr('data-url');
+                PaqPush && PaqPush('查看购物车', '');
+                Common.saveCartFromUrl(function () {
+                    location.href = _url;
+                });
+            });
+            //通过line联系供货商
+            $('body').on('click', '.j_goto_line', function () {
+                location.href = init_data.item.shop.line_url;
+            })
+        }
+    };
+    ITEM.init();
+})
