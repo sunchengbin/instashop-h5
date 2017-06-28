2c72631b4d49f55c70f8e492dd4995eb73d8cd62
sunchengbin <sunchengbin@weidian.com>
2017-06-28 13:59:01 +0800

    商品详情页

--- html/im.php
+++ html/im.php
@@ -13,7 +13,7 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
     <?=STATIC_ICO_CSS?>
     <?=STATIC_FONT_CSS?>
     <title></title>
-    <link href="<?=STATIC_HOST?>/css/dist/app/im_index.css?v=1498553909299" rel="stylesheet">
+    <link href="<?=STATIC_HOST?>/css/dist/app/im_index.css?v=1498629531428" rel="stylesheet">
 </head>
 <body>
     <nav class="header ks-clear">
