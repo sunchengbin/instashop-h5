cf0245084781272ed471299ebf3ff264aa37eaf0
sunchengbin <sunchengbin@weidian.com>
2017-06-29 16:52:08 +0800

    分销商订单确认页

--- static/js/app/distributororderconfirm.js
+++ static/js/app/distributororderconfirm.js
@@ -384,22 +384,27 @@ require([ 'dialog', 'ajax', 'config', 'base', 'common', 'btn', 'lang', 'fastclic
                     $('.j_shipper_name').addClass('class-error');
                     $(window).scrollTop($('.class-error').eq(0).offset().top);
                     return null;
-                    if(!_shipper_tel){
-                        $('.j_shipper_tel').addClass('class-error');
-                        $(window).scrollTop($('.class-error').eq(0).offset().top);
-                        return null;
-                    }
+                }
+                if(!_shipper_tel){
+                    $('.j_shipper_tel').addClass('class-error');
+                    $(window).scrollTop($('.class-error').eq(0).offset().top);
+                    return null;
                 }
             }
             if(!_name){
                 $('.j_name').addClass('class-error');
                 $(window).scrollTop($('.class-error').eq(0).offset().top);
                 return null;
-                if(!_telephone){
-                    $('.j_tel').addClass('class-error');
-                    $(window).scrollTop($('.class-error').eq(0).offset().top);
-                    return null;
-                }
+            }
+            if(!_telephone){
+                $('.j_tel').addClass('class-error');
+                $(window).scrollTop($('.class-error').eq(0).offset().top);
+                return null;
+            }
+            if(!_street){
+                $('.j_street').addClass('class-error');
+                $(window).scrollTop($('.class-error').eq(0).offset().top);
+                return null;
             }
             var _this = this;
             if (!_company && _this.logistics) {
