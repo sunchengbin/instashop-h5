2dcd9a98e2da694f7c87b04a6510ca9deacb7438
sunchengbin <sunchengbin@weidian.com>
2017-07-07 09:49:20 +0800

    修改包邮下单bug

--- html/usertest.php
+++ html/usertest.php
@@ -145,6 +145,6 @@ include_once( dirname(__FILE__).'/../html/router/common.php');
         <p>loading...</p>
     </section>
     <script src="<?=STATIC_HOST?>/js/base/require-zepto.js"></script>
-    <script src="<?=STATIC_HOST?>/js/dist/app/usertest.js?v=1499336470112"></script>
+    <script src="<?=STATIC_HOST?>/js/dist/app/usertest.js?v=1499392149532"></script>
 </body>
 </html>
